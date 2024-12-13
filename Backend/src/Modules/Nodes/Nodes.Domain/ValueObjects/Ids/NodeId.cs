d15df8b7967418689aea1b994b17f70de7592bef
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2024-12-13 11:12:23 +0100

    feat: create id class feat: create domain exception class

---
+++ Backend/src/Modules/Nodes/Nodes.Domain/ValueObjects/Ids/NodeId.cs
@@ -0,0 +1,22 @@
+﻿using Nodes.Domain.Exceptions;
+
+namespace Nodes.Domain.ValueObjects.Ids
+{
+    public record NodeId
+    {
+        private NodeId(Guid value)
+        {
+            Value = value;
+        }
+
+        public Guid Value { get; }
+
+        public static NodeId Of(Guid value)
+        {
+            ArgumentNullException.ThrowIfNull(value);
+            if (value == Guid.Empty) throw new DomainException("NodeId cannot be empty.");
+
+            return new NodeId(value);
+        }
+    }
+}
