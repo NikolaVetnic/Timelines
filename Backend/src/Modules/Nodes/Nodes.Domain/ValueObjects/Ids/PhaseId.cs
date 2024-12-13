42ae7ee3e871291fb935184c91ba2f39b7404be0
Hunor Tot-Bagi <hunortotbagi@gmail.com>
2024-12-13 14:28:34 +0100

    feat: create `PhaseId` class

---
+++ Backend/src/Modules/Nodes/Nodes.Domain/ValueObjects/Ids/PhaseId.cs
@@ -0,0 +1,22 @@
+﻿using BuildingBlocks.Domain.Exceptions;
+
+namespace Nodes.Domain.ValueObjects.Ids
+{
+    public record PhaseId
+    {
+        private PhaseId(Guid value)
+        {
+            Value = value;
+        }
+
+        public Guid Value { get; }
+
+        public static PhaseId Of(Guid value)
+        {
+            ArgumentNullException.ThrowIfNull(value);
+            if (value == Guid.Empty) throw new DomainException("PhaseId cannot be empty.");
+
+            return new PhaseId(value);
+        }
+    }
+}
