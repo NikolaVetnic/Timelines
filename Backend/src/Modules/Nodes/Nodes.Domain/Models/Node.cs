7e41898bdace606d5a95d836607d4911f573c475
NikolaVetnic <nikola.pacek.vetnic@gmail.com>
2024-12-13 13:36:04 +0100

    feat: add domain methods for working with categories and tags

--- Backend/src/Modules/Nodes/Nodes.Domain/Models/Node.cs
+++ Backend/src/Modules/Nodes/Nodes.Domain/Models/Node.cs
@@ -4,10 +4,35 @@ namespace Nodes.Domain.Models;
 
 public class Node : Aggregate<NodeId>
     {
+    private readonly List<string> _categories = [];
+    private readonly List<string> _tags = [];
+
+    public IReadOnlyList<string> Categories => _categories.AsReadOnly();
+    public IReadOnlyList<string> Tags => _tags.AsReadOnly();
+
         public required string Title { get; set; }
         public required string Description { get; set; }
         public required string Phase { get; set; }
-        public required string[] Categories { get; set; }
-        public required string[] Tags { get; set; }
+    #region Categories
+    private void AddCategory(string category)
+    {
+        _categories.Add(category);
     }
-}
+
+    private void RemoveCategory(string category)
+    {
+        _categories.Remove(category);
+    }
+    #endregion
+    
+    #region Tags
+    private void AddTag(string tag)
+    {
+        _tags.Add(tag);
+    }
+    
+    private void RemoveTag(string tag)
+    {
+        _tags.Remove(tag);
+    }
+    #endregion
