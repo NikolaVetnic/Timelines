b235c85a46fa5ba567fa2d1a0a961a12eb3c4b41
NikolaVetnic <nikola.pacek.vetnic@gmail.com>
2024-12-13 13:49:14 +0100

    feat: add timestamp and importance fields

--- Backend/src/Modules/Nodes/Nodes.Domain/Models/Node.cs
+++ Backend/src/Modules/Nodes/Nodes.Domain/Models/Node.cs
@@ -12,42 +12,52 @@ public class Node : Aggregate<NodeId>
 
     public required string Title { get; set; }
     public required string Description { get; set; }
+    public required DateTime Timestamp { get; set; }
+    public required int Importance { get; set; }
     public required string Phase { get; set; }
 
     #region Node
+
     public static Node Create(NodeId id, string title, string description, string phase,
-        List<string> categories, List<string> tags)
+        DateTime timestamp, int importance, List<string> categories, List<string> tags)
     {
         var node = new Node
         {
             Id = id,
             Title = title,
             Description = description,
+            Timestamp = timestamp,
+            Importance = importance,
             Phase = phase
         };
-        
+
         foreach (var category in categories)
             node.AddCategory(category);
 
         foreach (var tag in tags)
             node.AddTag(tag);
-        
+
         node.AddDomainEvent(new NodeCreatedEvent(node));
 
         return node;
     }
 
-    public void Update(string title, string description, string phase)
+    public void Update(string title, string description, DateTime timestamp, 
+        int importance, string phase)
     {
         Title = title;
         Description = description;
+        Timestamp = timestamp;
+        Importance = importance;
         Phase = phase;
-        
+
         AddDomainEvent(new NodeUpdatedEvent(this));
     }
+
     #endregion
 
     #region Categories
+
     private void AddCategory(string category)
     {
         _categories.Add(category);
@@ -57,17 +67,20 @@ public class Node : Aggregate<NodeId>
     {
         _categories.Remove(category);
     }
+
     #endregion
-    
+
     #region Tags
+
     private void AddTag(string tag)
     {
         _tags.Add(tag);
     }
-    
+
     private void RemoveTag(string tag)
     {
         _tags.Remove(tag);
     }
+
     #endregion
 }
