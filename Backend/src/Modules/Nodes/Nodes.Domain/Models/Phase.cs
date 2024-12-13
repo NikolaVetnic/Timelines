9f7c2074967331d666f0dd2613123ab014dc0e9f
Hunor Tot-Bagi <hunortotbagi@gmail.com>
2024-12-13 14:40:25 +0100

    chore: correct field names and spacing

--- Backend/src/Modules/Nodes/Nodes.Domain/Models/Phase.cs
+++ Backend/src/Modules/Nodes/Nodes.Domain/Models/Phase.cs
@@ -7,12 +7,12 @@ namespace Nodes.Domain.Models
     {
         public required string Title { get; set; }
         public required string Description { get; set; }
-        public required  DateTime StartDate { get; set; }
-        public required  DateTime? EndDate { get; set; }
+        public required DateTime StartDate { get; set; }
+        public required DateTime? EndDate { get; set; }
         public required TimeSpan? Duration { get; set; }
         public required string Status { get; set; }
         public required decimal Progress { get; set; }
-        public required bool IsComplete { get; set; }
+        public required bool IsCompleted { get; set; }
         public required PhaseId Parent { get; set; }
         public required PhaseId[] DependsOn { get; set; }
         public required string AssignedTo { get; set; }
