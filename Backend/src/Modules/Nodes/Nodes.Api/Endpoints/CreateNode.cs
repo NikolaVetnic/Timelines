dc1427103efc181e0a0da62d80de41b63ea7860a
NikolaVetnic <nikola.pacek.vetnic@gmail.com>
2024-12-15 22:37:43 +0100

    feat: implement create node endpoint and register api layer in the main module

---
+++ Backend/src/Modules/Nodes/Nodes.Api/Endpoints/CreateNode.cs
@@ -0,0 +1,35 @@
+using BuildingBlocks.Domain.ValueObjects.Ids;
+using Carter;
+using Mapster;
+using MediatR;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using Nodes.Application.Dtos;
+using Nodes.Application.Nodes.Commands.CreateNode;
+
+namespace Nodes.Api.Endpoints;
+
+public class CreateNode : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapPost("/Nodes", async (CreateNodeRequest request, ISender sender) =>
+            {
+                var command = request.Adapt<CreateNodeCommand>();
+                var result = await sender.Send(command);
+                var response = result.Adapt<CreateNodeResponse>();
+
+                return Results.Created($"/Nodes/{response.Id}", response);
+            })
+            .WithName("CreateNode")
+            .Produces<CreateNodeResponse>(StatusCodes.Status201Created)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .WithSummary("Create Node")
+            .WithDescription("Creates a new node");
+    }
+}
+
+public record CreateNodeRequest(NodeDto Node);
+
+public record CreateNodeResponse(NodeId Id);
--- Backend/src/Modules/Nodes/Nodes.Api/Extensions/ServiceCollectionExtensions.cs
+++ Backend/src/Modules/Nodes/Nodes.Api/Extensions/ServiceCollectionExtensions.cs
@@ -1,19 +1,30 @@
+using Carter;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Nodes.Infrastructure;
 
 namespace Nodes.Api.Extensions;
 
 public static class ServiceCollectionExtensions
 {
-    public static IServiceCollection AddNodesModule(this IServiceCollection services)
+    public static IServiceCollection AddNodesModule
+        (this IServiceCollection services, IConfiguration configuration)
     {
+        services.AddCarter();
+        
+        services.AddApplicationLayer();
+        services.AddNodesInfrastructureServices(configuration);
+        
         return services;
     }
 
     public static IEndpointRouteBuilder MapNodesModuleEndpoints(this IEndpointRouteBuilder endpoints)
     {
         endpoints.MapGet("/Nodes", () => "Hello World! This is Timelines Nodes module.");
+        endpoints.MapCarter();
+        
         return endpoints;
     }
 }
