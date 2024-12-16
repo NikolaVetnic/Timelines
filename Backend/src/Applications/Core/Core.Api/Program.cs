81be6cd00d1e39e27c6912e1119a91de44cd0024
NikolaVetnic <nikola.pacek.vetnic@gmail.com>
2024-12-16 09:34:22 +0100

    refactor: extract module calls to extensions

--- Backend/src/Applications/Core/Core.Api/Program.cs
+++ Backend/src/Applications/Core/Core.Api/Program.cs
@@ -1,23 +1,13 @@
 using BuildingBlocks.Domain;
-using BuildingBlocks.Infrastructure.Extensions;
+using Core.Api.Extensions;
 using HealthChecks.UI.Client;
 using Microsoft.AspNetCore.Diagnostics.HealthChecks;
-using Files.Api.Extensions;
-using Nodes.Api.Extensions;
-using Notes.Api.Extensions;
-using Reminders.Api.Extensions;
-using Timelines.Api.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container
 builder.Services.AddControllers();
-
-builder.Services.AddFilesModule();
-builder.Services.AddNodesModule(builder.Configuration);
-builder.Services.AddNotesModule();
-builder.Services.AddRemindersModule();
-builder.Services.AddTimelinesModule();
+builder.Services.AddModules(builder.Configuration);
 
 builder.Services.AddHealthChecks()
     .AddNpgSql(builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException());
@@ -27,12 +17,7 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 app.UseRouting();
 app.MapControllers();
-
-app.MapFilesModuleEndpoints();
-app.MapNodesModuleEndpoints();
-app.MapNotesModuleEndpoints();
-app.MapRemindersModuleEndpoints();
-app.MapTimelinesModuleEndpoints();
+app.UseModules();
 
 app.MapGet("/", BuildingBlocksTestClass.GetTestString);
 
