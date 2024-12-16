2764a8f3a4bb03e144ab70bbf579ea9e99ff6ef5
NikolaVetnic <nikola.pacek.vetnic@gmail.com>
2024-12-16 12:30:16 +0100

    chore: use file-scoped namespaces

--- Backend/src/BuildingBlocks/BuildingBlocks.Domain/Abstractions/Entity.cs
+++ Backend/src/BuildingBlocks/BuildingBlocks.Domain/Abstractions/Entity.cs
@@ -1,11 +1,10 @@
-﻿namespace BuildingBlocks.Domain.Abstractions
+﻿namespace BuildingBlocks.Domain.Abstractions;
+
+public abstract class Entity<T> : IEntity<T>
 {
-    public abstract class Entity<T> : IEntity<T>
-    {
-        public required T Id { get; init; }
-        public DateTime? CreatedAt { get; set; }
-        public string? CreatedBy { get; set; }
-        public DateTime? LastModifiedAt { get; set; }
-        public string? LastModifiedBy { get; set; }
-    }
+    public required T Id { get; init; }
+    public DateTime? CreatedAt { get; set; }
+    public string? CreatedBy { get; set; }
+    public DateTime? LastModifiedAt { get; set; }
+    public string? LastModifiedBy { get; set; }
 }
--- Backend/src/BuildingBlocks/BuildingBlocks.Domain/Abstractions/IEntity.cs
+++ Backend/src/BuildingBlocks/BuildingBlocks.Domain/Abstractions/IEntity.cs
@@ -1,15 +1,14 @@
-﻿namespace BuildingBlocks.Domain.Abstractions
-{
-    public interface IEntity<out T> : IEntity
-    {
-        T Id { get; }
-    }
+﻿namespace BuildingBlocks.Domain.Abstractions;
 
-    public interface IEntity
-    {
-        public DateTime? CreatedAt { get; set; }
-        public string? CreatedBy { get; set; }
-        public DateTime? LastModifiedAt { get; set; }
-        public string? LastModifiedBy { get; set; }
-    }
+public interface IEntity<out T> : IEntity
+{
+    T Id { get; }
+}
+
+public interface IEntity
+{
+    public DateTime? CreatedAt { get; set; }
+    public string? CreatedBy { get; set; }
+    public DateTime? LastModifiedAt { get; set; }
+    public string? LastModifiedBy { get; set; }
 }
