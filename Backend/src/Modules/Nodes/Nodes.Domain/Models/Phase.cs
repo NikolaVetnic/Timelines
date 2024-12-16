7a10321d33cf4ca1471ada434a86d45b1be98363
Hunor Tot-Bagi <hunortotbagi@gmail.com>
2024-12-16 10:39:33 +0100

    chore: utilize file-scoped namespaces

--- Backend/src/Modules/Nodes/Nodes.Domain/Models/Phase.cs
+++ Backend/src/Modules/Nodes/Nodes.Domain/Models/Phase.cs
@@ -1,22 +1,21 @@
 ﻿using BuildingBlocks.Domain;
 using Nodes.Domain.ValueObjects.Ids;
 
-namespace Nodes.Domain.Models
+namespace Nodes.Domain.Models;
+
+public class Phase : Entity<PhaseId>
 {
-    public class Phase : Entity<PhaseId>
-    {
-        public required string Title { get; set; }
-        public required string Description { get; set; }
-        public required DateTime StartDate { get; set; }
-        public required DateTime? EndDate { get; set; }
-        public required TimeSpan? Duration { get; set; }
-        public required string Status { get; set; }
-        public required decimal Progress { get; set; }
-        public required bool IsCompleted { get; set; }
-        public required PhaseId Parent { get; set; }
-        public required PhaseId[] DependsOn { get; set; }
-        public required string AssignedTo { get; set; }
-        public required string[] Stakeholders { get; set; }
-        public required string[] Tags { get; set; }
-    }
+    public required string Title { get; set; }
+    public required string Description { get; set; }
+    public required DateTime StartDate { get; set; }
+    public required DateTime? EndDate { get; set; }
+    public required TimeSpan? Duration { get; set; }
+    public required string Status { get; set; }
+    public required decimal Progress { get; set; }
+    public required bool IsCompleted { get; set; }
+    public required PhaseId Parent { get; set; }
+    public required PhaseId[] DependsOn { get; set; }
+    public required string AssignedTo { get; set; }
+    public required string[] Stakeholders { get; set; }
+    public required string[] Tags { get; set; }
 }
--- Backend/src/Modules/Nodes/Nodes.Domain/ValueObjects/Ids/PhaseId.cs
+++ Backend/src/Modules/Nodes/Nodes.Domain/ValueObjects/Ids/PhaseId.cs
@@ -1,22 +1,21 @@
 ﻿using BuildingBlocks.Domain.Exceptions;
 
-namespace Nodes.Domain.ValueObjects.Ids
+namespace Nodes.Domain.ValueObjects.Ids;
+
+public record PhaseId
 {
-    public record PhaseId
+    private PhaseId(Guid value)
     {
-        private PhaseId(Guid value)
-        {
-            Value = value;
-        }
+        Value = value;
+    }
 
-        public Guid Value { get; }
+    public Guid Value { get; }
 
-        public static PhaseId Of(Guid value)
-        {
-            ArgumentNullException.ThrowIfNull(value);
-            if (value == Guid.Empty) throw new DomainException("PhaseId cannot be empty.");
+    public static PhaseId Of(Guid value)
+    {
+        ArgumentNullException.ThrowIfNull(value);
+        if (value == Guid.Empty) throw new DomainException("PhaseId cannot be empty.");
 
-            return new PhaseId(value);
-        }
+        return new PhaseId(value);
     }
 }
