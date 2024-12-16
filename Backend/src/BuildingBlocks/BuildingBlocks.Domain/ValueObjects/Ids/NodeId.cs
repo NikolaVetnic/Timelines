15adc66f5dc5bcf41082273f7c9c69b3d3eebf0a
NikolaVetnic <nikola.pacek.vetnic@gmail.com>
2024-12-16 10:30:21 +0100

    chore: use file-scoped namespace

--- Backend/src/BuildingBlocks/BuildingBlocks.Domain/ValueObjects/Ids/NodeId.cs
+++ Backend/src/BuildingBlocks/BuildingBlocks.Domain/ValueObjects/Ids/NodeId.cs
@@ -1,21 +1,20 @@
 ﻿using BuildingBlocks.Domain.Exceptions;
 
-namespace BuildingBlocks.Domain.ValueObjects.Ids
+namespace BuildingBlocks.Domain.ValueObjects.Ids;
+
+public record NodeId
 {
-    public record NodeId
+    private NodeId(Guid value)
     {
-        private NodeId(Guid value)
-        {
-            Value = value;
-        }
+        Value = value;
+    }
 
-        public Guid Value { get; }
+    public Guid Value { get; }
 
-        public static NodeId Of(Guid value)
-        {
-            if (value == Guid.Empty) throw new DomainException("NodeId cannot be empty.");
+    public static NodeId Of(Guid value)
+    {
+        if (value == Guid.Empty) throw new DomainException("NodeId cannot be empty.");
 
-            return new NodeId(value);
-        }
+        return new NodeId(value);
     }
 }
