8ff8385b1aa4a5433cc81c07b2aa6a6264aee31c
Hunor Tot-Bagi <hunortotbagi@gmail.com>
2024-12-25 12:09:01 +0100

    feat: add `FileId`

---
+++ Backend/src/BuildingBlocks/BuildingBlocks.Domain/ValueObjects/Ids/FileId.cs
@@ -0,0 +1,20 @@
+﻿using BuildingBlocks.Domain.Exceptions;
+
+namespace BuildingBlocks.Domain.ValueObjects.Ids;
+
+public record FileId
+{
+    private FileId(Guid value)
+    {
+        Value = value;
+    }
+
+    public Guid Value { get; }
+
+    public static FileId Of(Guid value)
+    {
+        if (value == Guid.Empty) throw new DomainException("FileId cannot be empty.");
+
+        return new FileId(value);
+    }
+}
