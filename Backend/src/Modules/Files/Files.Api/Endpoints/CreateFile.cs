83dc878fa4e800a9ef08628f145483edd533922f
Hunor Tot-Bagi <hunortotbagi@gmail.com>
2024-12-25 12:05:06 +0100

    feat: create initial setup

---
+++ Backend/src/Modules/Files/Files.Api/Endpoints/CreateFile.cs
@@ -0,0 +1,29 @@
+﻿using Carter;
+using MediatR;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+
+namespace Files.Api.Endpoints;
+
+public class CreateFile : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapPost("/Nodes", async (CreateFileRequest request, ISender sender) =>
+            {
+                var command = request.Adapt<CreateFileCommand>();
+                var result = await sender.Send(command);
+                var response = result.Adapt<CreateFileResponse>();
+
+                return Results.Created($"/Nodes/{response.Id}", response);
+            })
+            .WithName("CreateFile")
+            .Produces<CreateFileResponse>(StatusCodes.Status201Created)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .WithSummary("Create File")
+            .WithDescription("Creates a new file");
+    }
+}
+
+public record CreateFileRequest(FileDto File);
