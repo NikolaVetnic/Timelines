165b2ccb7a6be537dad0abac71b3300b1f15345d
Hunor Tot-Bagi <hunortotbagi@gmail.com>
2024-12-25 10:27:51 +0100

    chore: fix seeding data

--- Backend/src/Modules/Reminders/Reminders.Infrastructure/Data/Extensions/InitialData.cs
+++ Backend/src/Modules/Reminders/Reminders.Infrastructure/Data/Extensions/InitialData.cs
@@ -7,10 +7,10 @@ internal static class InitialData
         {
             Reminder.Create(
                 ReminderId.Of(Guid.Parse("2df76835-c92b-45d0-9232-61901c4abe97")),
-                "Meeting Room 1 - Daily", "Important meeting with Timo", DateTime.UtcNow, "High", DateTime.Now, "Pending"),
+                "Meeting Room 1 - Daily", "Important meeting with Timo", DateTime.Now.AddHours(2), "High", DateTime.Now.AddHours(1), "Pending"),
 
             Reminder.Create(
                 ReminderId.Of(Guid.Parse("6968d886-9e39-4fc0-9f2c-a5fbc1548970")),
-                "Weekly Team Sync", "Basic team sync with the team", DateTime.Now, "Low", DateTime.Now, "Canceled"),
+                "Weekly Team Sync", "Basic team sync with the team", DateTime.Now.AddHours(2), "Low", DateTime.Now.AddHours(1), "Canceled"),
         };
 }
