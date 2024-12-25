9e2ef5f93482874ce3ed7bdf37d03c3aec534ee9
Hunor Tot-Bagi <hunortotbagi@gmail.com>
2024-12-25 10:25:11 +0100

    feat: add command validators

--- Backend/src/Modules/Reminders/Reminders.Application/Reminders/Commands/CreateReminder/CreateReminderCommand.cs
+++ Backend/src/Modules/Reminders/Reminders.Application/Reminders/Commands/CreateReminder/CreateReminderCommand.cs
@@ -12,8 +12,25 @@ public class CreateReminderCommandValidator : AbstractValidator<CreateReminderCo
 {
     public CreateReminderCommandValidator()
     {
-        RuleFor(x => x.Reminder.Title).NotEmpty().WithMessage("Title is required.");
+        RuleFor(x => x.Reminder.Title)
+            .NotEmpty().WithMessage("Title is required.")
+            .MaximumLength(100).WithMessage("Title must not exceed 100 characters.");
 
-        // ToDo: Add remaining Node command validators
+        RuleFor(x => x.Reminder.Description)
+            .NotEmpty().WithMessage("Description is required.")
+            .MaximumLength(500).WithMessage("Description must not exceed 500 characters.");
+
+        RuleFor(x => x.Reminder.DueDateTime)
+            .GreaterThan(DateTime.Now).WithMessage("Due date and time must be in the future.");
+
+        RuleFor(x => x.Reminder.Priority)
+            .NotEmpty().WithMessage("Priority is required.");
+
+        RuleFor(x => x.Reminder.NotificationTime)
+            .LessThan(x => x.Reminder.DueDateTime).WithMessage("Notification time must be before the due date and time.")
+            .GreaterThan(DateTime.Now).WithMessage("Notification time must be in the future.");
+
+        RuleFor(x => x.Reminder.Status)
+            .NotEmpty().WithMessage("Status is required.");
     }
 }
