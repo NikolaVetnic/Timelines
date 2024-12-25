03c98cb9bf2ba23be4fcfcd4d51e066b666d344e
Hunor Tot-Bagi <hunortotbagi@gmail.com>
2024-12-25 15:59:59 +0100

    chore: delete unused domain test class

--- Backend/src/Modules/Reminders/Reminders.Domain/ReminderDomainTestClass.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Reminders.Domain;
-
-public class ReminderDomainTestClass
-{
-    public static string GetTestString()
-    {
-        return "Hello World! This is Timelines Reminders module.";
-    }
-}
