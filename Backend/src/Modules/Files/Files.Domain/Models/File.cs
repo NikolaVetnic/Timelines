0a2000af3837ce985494e1dcc6e2ef18227aab8e
Hunor Tot-Bagi <hunortotbagi@gmail.com>
2024-12-25 12:17:08 +0100

    feat: implement `File` data model alonside with `Create` and `Update` method

--- Backend/src/Modules/Files/Files.Domain/Models/File.cs
+++ Backend/src/Modules/Files/Files.Domain/Models/File.cs
@@ -1,12 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using BuildingBlocks.Domain.Abstractions;
+using BuildingBlocks.Domain.ValueObjects.Ids;
+using Files.Domain.Events;
 
-namespace Files.Domain.Models
+namespace Files.Domain.Models;
+
+public class File : Aggregate<FileId>
 {
-    internal class File
+    public required string Title { get; set; }
+
+    #region File
+
+    public static File Create(FileId id, string title)
     {
+        var file = new File
+        {
+            Id = id,
+            Title = title
+        };
+
+        file.AddDomainEvent(new FileCreatedEvent(file));
+
+        return file;
     }
+
+    public void Update(string title)
+    {
+        Title = title;
+
+        AddDomainEvent(new FileUpdatedEvent(this));
+    }
+
+    #endregion
 }
