3d892beb682772ab4d94d58a0d91953c99232199
Hunor Tot-Bagi <hunortotbagi@gmail.com>
2024-12-26 10:03:43 +0100

    chore: update database seeding

--- Backend/src/Modules/Files/Files.Infrastructure/Data/Extensions/InitialData.cs
+++ Backend/src/Modules/Files/Files.Infrastructure/Data/Extensions/InitialData.cs
@@ -7,12 +7,22 @@ internal static class InitialData
         {
             FileAsset.Create(
                 FileAssetId.Of(Guid.Parse("2df76835-c92b-45d0-9232-61901c4abe97")),
-                "Title 1"
+                "Document 1",
+                1.5f,
+                "pdf",
+                "Owner1",
+                "Description for Document 1",
+                ["User1", "User2"]
             ),
 
             FileAsset.Create(
                 FileAssetId.Of(Guid.Parse("6968d886-9e39-4fc0-9f2c-a5fbc1548970")),
-                "Title 2"
+                "Document 2",
+                2.3f,
+                "docx",
+                "Owner2",
+                "Description for Document 2",
+                ["User3", "User4"]
             )
         };
 }
