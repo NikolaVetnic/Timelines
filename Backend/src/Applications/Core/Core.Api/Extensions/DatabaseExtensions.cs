71af44124d4b1619d2e56fa9875fd18ee1900f5c
Hunor Tot-Bagi <hunortotbagi@gmail.com>
2024-12-26 20:36:43 +0100

    chore: adjust method order to follow lexographic order

--- Backend/src/Applications/Core/Core.Api/Extensions/DatabaseExtensions.cs
+++ Backend/src/Applications/Core/Core.Api/Extensions/DatabaseExtensions.cs
@@ -8,8 +8,8 @@ public static class DatabaseExtensions
 {
     public static async Task MigrateAndSeedAllModulesAsync(this IServiceProvider services)
     {
+        await services.MigrateAndSeedFilesDatabaseAsync();
         await services.MigrateAndSeedNodesDatabaseAsync();
         await services.MigrateAndSeedRemindersDatabaseAsync();
-        await services.MigrateAndSeedFilesDatabaseAsync();
     }
 }
