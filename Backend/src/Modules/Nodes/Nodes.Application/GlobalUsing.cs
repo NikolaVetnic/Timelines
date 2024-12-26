984278672b5ef7d3571d636169ed3c8d01a60ae1
NikolaVetnic <nikola.pacek.vetnic@gmail.com>
2024-12-26 12:05:39 +0100

    chore: update global using

--- Backend/src/Modules/Nodes/Nodes.Application/GlobalUsing.cs
+++ Backend/src/Modules/Nodes/Nodes.Application/GlobalUsing.cs
@@ -1,6 +1,7 @@
 global using FluentValidation;
 global using Microsoft.EntityFrameworkCore;
 global using BuildingBlocks.Application.Cqrs;
+global using BuildingBlocks.Application.Exceptions;
 global using BuildingBlocks.Domain.ValueObjects.Ids;
 global using Nodes.Application.Dtos;
 global using Nodes.Domain.Models;
