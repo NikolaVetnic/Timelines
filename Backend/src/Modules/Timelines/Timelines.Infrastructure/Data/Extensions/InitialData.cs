e8784af2853c6e0665f8c50567bce738d7f18e91
Hunor Tot-Bagi <hunortotbagi@gmail.com>
2024-12-27 14:57:09 +0100

    refactor: use a distinct `Guid` to avoid potential conflicts with other modules

--- Backend/src/Modules/Timelines/Timelines.Infrastructure/Data/Extensions/InitialData.cs
+++ Backend/src/Modules/Timelines/Timelines.Infrastructure/Data/Extensions/InitialData.cs
@@ -6,11 +6,11 @@ internal static class InitialData
         new List<Timeline>
         {
             Timeline.Create(
-                TimelineId.Of(Guid.Parse("2df76835-c92b-45d0-9232-61901c4abe97")),
+                TimelineId.Of(Guid.Parse("d87c1c54-b287-4a82-b45b-65db1d71e2fd")),
                 "Timeline 1"),
 
             Timeline.Create(
-                TimelineId.Of(Guid.Parse("6968d886-9e39-4fc0-9f2c-a5fbc1548970")),
+                TimelineId.Of(Guid.Parse("f739b2c7-883e-46c6-917c-d29114e3e017")),
                 "Timeline 2")
         };
 }
