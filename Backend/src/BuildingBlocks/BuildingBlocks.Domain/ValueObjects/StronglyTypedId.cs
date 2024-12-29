dd448c407bbd5730c46d1383b5c6530f171f70db
Hunor Tot-Bagi <hunortotbagi@gmail.com>
2024-12-29 16:17:19 +0100

    feat: add `StronglyTypeId`

---
+++ Backend/src/BuildingBlocks/BuildingBlocks.Domain/ValueObjects/StronglyTypedId.cs
@@ -0,0 +1,17 @@
+﻿namespace BuildingBlocks.Domain.ValueObjects;
+
+public abstract record StronglyTypedId<T>
+    where T : StronglyTypedId<T>
+{
+    protected StronglyTypedId(Guid value)
+    {
+        if (value == Guid.Empty)
+            throw new ArgumentException($"{typeof(T).Name} cannot be empty.", nameof(value));
+
+        Value = value;
+    }
+
+    public Guid Value { get; }
+
+    public override string ToString() => Value.ToString();
+}
