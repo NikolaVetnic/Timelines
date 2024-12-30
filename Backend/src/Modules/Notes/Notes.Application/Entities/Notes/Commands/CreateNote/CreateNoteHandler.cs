90f9e3322f06c48053d26d363b77707f9d966f17
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2024-12-30 17:12:52 +0100

    refactore: improve code structure of CreateNoteHandler

--- Backend/src/Modules/Notes/Notes.Application/Entities/Notes/Commands/CreateNote/CreateNoteHandler.cs
+++ Backend/src/Modules/Notes/Notes.Application/Entities/Notes/Commands/CreateNote/CreateNoteHandler.cs
@@ -5,17 +5,25 @@ public class CreateNoteHandler(INotesDbContext dbContext)
 {
     public async Task<CreateNoteResult> Handle(CreateNoteCommand command, CancellationToken cancellationToken)
     {
-        var note = Note.Create(
+        var note = command.ToNote();
+
+        dbContext.Notes.Add(note);
+        await dbContext.SaveChangesAsync(cancellationToken);
+
+        return new CreateNoteResult(note.Id);
+    }
+}
+
+internal static class CreateNoteCommandExtensions
+{
+    public static Note ToNote(this CreateNoteCommand command)
+    {
+        return Note.Create(
             NoteId.Of(Guid.NewGuid()),
             command.Note.Title,
             command.Note.Content,
             command.Note.Timestamp,
             command.Note.Importance
         );
-
-        dbContext.Notes.Add(note);
-        await dbContext.SaveChangesAsync(cancellationToken);
-
-        return new CreateNoteResult(note.Id);
     }
 }
