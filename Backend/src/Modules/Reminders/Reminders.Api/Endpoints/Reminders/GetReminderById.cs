d4d4031a5d348b22154f11c570a4e74b87d9eb9d
Hunor Tot-Bagi <hunortotbagi@gmail.com>
2024-12-30 15:32:28 +0100

    feat: implement endpoints

---
+++ Backend/src/Modules/Reminders/Reminders.Api/Endpoints/Reminders/GetReminderById.cs
@@ -0,0 +1,29 @@
+﻿using Reminders.Application.Entities.Reminders.Queries.GetReminderById;
+using System.Text.Json.Serialization;
+
+namespace Reminders.Api.Endpoints.Reminders;
+
+// ReSharper disable once UnusedType.Global
+public class GetReminderById : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/Reminders/{reminderId}", async (string nodeId, ISender sender) =>
+            {
+                var result = await sender.Send(new GetReminderByIdQuery(nodeId));
+                var response = result.Adapt<GetReminderByIdResponse>();
+
+                return Results.Ok(response);
+            })
+            .WithName("GetReminderById")
+            .Produces<GetReminderByIdResponse>()
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .ProducesProblem(StatusCodes.Status404NotFound)
+            .WithSummary("Get Reminder by Id")
+            .WithDescription("Get Reminder by Id");
+    }
+}
+
+// ReSharper disable once ClassNeverInstantiated.Global
+// ReSharper disable once NotAccessedPositionalProperty.Global
+public record GetReminderByIdResponse([property: JsonPropertyName("reminder")] ReminderDto ReminderDto);
