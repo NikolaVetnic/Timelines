888e4f515e3019474ae8cc0a1dead1ffa0005e30
Hunor Tot-Bagi <hunortotbagi@gmail.com>
2024-12-30 15:40:46 +0100

    chore: rename `request` to `query`

--- Backend/src/Modules/Reminders/Reminders.Application/Entities/Reminders/Queries/GetReminderById/GetReminderByIdHandler.cs
+++ Backend/src/Modules/Reminders/Reminders.Application/Entities/Reminders/Queries/GetReminderById/GetReminderByIdHandler.cs
@@ -5,14 +5,14 @@ namespace Reminders.Application.Entities.Reminders.Queries.GetReminderById;
 
 internal class GetReminderByIdHandler(IRemindersDbContext dbContext) : IQueryHandler<GetReminderByIdQuery, GetReminderByIdResult>
 {
-    public async Task<GetReminderByIdResult> Handle(GetReminderByIdQuery request, CancellationToken cancellationToken)
+    public async Task<GetReminderByIdResult> Handle(GetReminderByIdQuery query, CancellationToken cancellationToken)
     {
         var reminder = await dbContext.Reminders
             .AsNoTracking()
-            .SingleOrDefaultAsync(r => r.Id == ReminderId.Of(Guid.Parse(request.Id)), cancellationToken);
+            .SingleOrDefaultAsync(r => r.Id == ReminderId.Of(Guid.Parse(query.Id)), cancellationToken);
 
         if (reminder is null)
-            throw new ReminderNotFoundException(request.Id);
+            throw new ReminderNotFoundException(query.Id);
 
         return new GetReminderByIdResult(reminder.ToReminderDto());
     }
