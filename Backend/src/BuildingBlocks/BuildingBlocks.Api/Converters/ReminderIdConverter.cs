5e502448ab393e91a9ca158c10871c195c19bd80
Hunor Tot-Bagi <hunortotbagi@gmail.com>
2024-12-31 15:12:53 +0100

    chore: remove unused `ReminderIdConverter`

--- Backend/src/BuildingBlocks/BuildingBlocks.Api/Converters/ReminderIdConverter.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace BuildingBlocks.Api.Converters;
-
-public class ReminderIdConverter : IRegister
-{
-    public void Register(TypeAdapterConfig config) =>
-        config.NewConfig<ReminderId, ReminderId>().ConstructUsing(src => ReminderId.Of(src.Value));
-}
