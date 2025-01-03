a181348bc919e24b210419f9e445aa803b0054af
Hunor Tot-Bagi <hunortotbagi@gmail.com>
2025-01-03 11:59:10 +0100

    fix: re-implement node validator

---
+++ Backend/src/BuildingBlocks/BuildingBlocks.Api/Converters/NodeIdConverter.cs
@@ -0,0 +1,10 @@
+﻿using BuildingBlocks.Domain.ValueObjects.Ids;
+using Mapster;
+
+namespace BuildingBlocks.Api.Converters;
+
+public class NodeIdConverter : IRegister
+{
+    public void Register(TypeAdapterConfig config) =>
+        config.NewConfig<NodeId, NodeId>().ConstructUsing(src => NodeId.Of(src.Value));
+}
