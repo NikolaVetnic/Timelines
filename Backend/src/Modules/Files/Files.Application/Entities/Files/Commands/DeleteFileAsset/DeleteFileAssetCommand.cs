64c274b0124f32e4e74a720f17f4d1bb4e43bd58
Hunor Tot-Bagi <hunortotbagi@gmail.com>
2025-01-04 20:39:41 +0100

    feat: implement delete file

---
+++ Backend/src/Modules/Files/Files.Application/Entities/Files/Commands/DeleteFileAsset/DeleteFileAssetCommand.cs
@@ -0,0 +1,13 @@
+﻿namespace Files.Application.Entities.Files.Commands.DeleteFileAsset;
+
+public record DeleteFileAssetCommand(string FileAssetId) : ICommand<DeleteFileAssetResult>;
+
+public record DeleteFileAssetResult(bool IsSuccess);
+
+public class DeleteFileAssetCommandValidator : AbstractValidator<DeleteFileAssetCommand>
+{
+    public DeleteFileAssetCommandValidator()
+    {
+        RuleFor(x => x.FileAssetId).NotEmpty().WithMessage("FileAssetId is required");
+    }
+}
---
+++ Backend/src/Modules/Files/Files.Application/Entities/Files/Commands/DeleteFileAsset/DeleteFileAssetHandler.cs
@@ -0,0 +1,22 @@
+﻿using Files.Application.Entities.Files.Exceptions;
+
+namespace Files.Application.Entities.Files.Commands.DeleteFileAsset;
+
+public class DeleteFileAssetHandler(IFilesDbContext dbContext) : ICommandHandler<DeleteFileAssetCommand, DeleteFileAssetResult>
+{
+    public async Task<DeleteFileAssetResult> Handle(DeleteFileAssetCommand command, CancellationToken cancellationToken)
+    {
+        var fileAsset = await dbContext.FileAssets
+            .AsNoTracking()
+            .SingleOrDefaultAsync(f => f.Id == FileAssetId.Of(Guid.Parse(command.FileAssetId)), cancellationToken);
+
+        if (fileAsset is null)
+            throw new FileAssetNotFoundException(command.FileAssetId);
+
+
+        dbContext.FileAssets.Remove(fileAsset);
+        await dbContext.SaveChangesAsync(cancellationToken);
+
+        return new DeleteFileAssetResult(true);
+    }
+}
