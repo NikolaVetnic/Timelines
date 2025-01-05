9229e77f4edbf886d87cc5f8d3ae4163c04c4441
Hunor Tot-Bagi <hunortotbagi@gmail.com>
2025-01-05 08:29:55 +0100

    feat: implement delete `Timeline` endpoint

---
+++ Backend/src/Modules/Timelines/Timelines.Application/Entities/Timelines/Commands/DeleteTimeline/DeleteTimelineCommand.cs
@@ -0,0 +1,13 @@
+﻿namespace Timelines.Application.Entities.Timelines.Commands.DeleteTimeline;
+
+public record DeleteTimelineCommand(string TimelineId) : ICommand<DeleteTimelineResult>;
+
+public record DeleteTimelineResult(bool TimelineDeleted);
+
+public class DeleteTimelineCommandValidator : AbstractValidator<DeleteTimelineCommand>
+{
+    public DeleteTimelineCommandValidator()
+    {
+        RuleFor(x => x.TimelineId).NotEmpty().WithMessage("TimelineId is required");
+    }
+}
---
+++ Backend/src/Modules/Timelines/Timelines.Application/Entities/Timelines/Commands/DeleteTimeline/DeleteTimelineHandler.cs
@@ -0,0 +1,22 @@
+﻿using Timelines.Application.Entities.Timelines.Exceptions;
+
+namespace Timelines.Application.Entities.Timelines.Commands.DeleteTimeline;
+
+public class DeleteNodeHandler(ITimelinesDbContext dbContext) : ICommandHandler<DeleteTimelineCommand, DeleteTimelineResult>
+{
+    public async Task<DeleteTimelineResult> Handle(DeleteTimelineCommand command, CancellationToken cancellationToken)
+    {
+        var timeline = await dbContext.Timelines
+        .AsNoTracking()
+            .SingleOrDefaultAsync(t => t.Id == TimelineId.Of(Guid.Parse(command.TimelineId)), cancellationToken);
+
+        if (timeline is null)
+            throw new TimelineNotFoundException(command.TimelineId);
+
+
+        dbContext.Timelines.Remove(timeline);
+        await dbContext.SaveChangesAsync(cancellationToken);
+
+        return new DeleteTimelineResult(true);
+    }
+}
