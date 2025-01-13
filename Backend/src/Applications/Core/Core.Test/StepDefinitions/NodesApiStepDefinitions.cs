44aa6c259c21fd6cddd5cd6335d389df828e1e24
NikolaVetnic <nikola.pacek.vetnic@gmail.com>
2025-01-13 08:28:22 +0100

    refactor: update step definitions to use sdk types

--- Backend/src/Applications/Core/Core.Test/StepDefinitions/NodesApiStepDefinitions.cs
+++ Backend/src/Applications/Core/Core.Test/StepDefinitions/NodesApiStepDefinitions.cs
@@ -1,8 +1,6 @@
-using System.Globalization;
-using Core.Api.Sdk;
-using CreateNodeRequest = Nodes.Api.Endpoints.Nodes.CreateNodeRequest;
-using NodeDto = Nodes.Application.Entities.Nodes.Dtos.NodeDto;
-using NodeId = BuildingBlocks.Domain.ValueObjects.Ids.NodeId;
+using SdkNodeId = Core.Api.Sdk.Contracts.Nodes.ValueObjects.NodeId;
+using SdkNodeDto = Core.Api.Sdk.Contracts.Nodes.Dtos.NodeDto;
+using SdkCreateNodeRequest = Core.Api.Sdk.Contracts.Nodes.Commands.CreateNodeRequest;
 
 // ReSharper disable ConvertToPrimaryConstructor
 // ReSharper disable InconsistentNaming
@@ -17,39 +15,39 @@ public class NodesApiStepDefinitions
     private readonly ICoreApiClient _apiApiClient;
     private HttpResponseMessage? _response;
 
-    private NodeDto? _nodeDto;
-    private NodeId? _persistedNodeId;
+    private SdkNodeDto? _nodeDto;
+    private SdkNodeId? _persistedNodeId;
 
     public NodesApiStepDefinitions(ICoreApiClient apiClient)
     {
         _apiApiClient = apiClient;
     }
 
-    #region Given
-
-    [Given("a Node")]
-    public void GivenANode()
-    {
-        _nodeDto = new NodeDto(
-            null, 
-            "Test Node", 
-            "Test Node Description.",
-            DateTime.Parse("2024-01-15T09:00:00.000000Z", null, DateTimeStyles.RoundtripKind), 
-            1, 
-            "Testing",
-            ["Category1", "Category2"], 
-            ["Tag1", "Tag2", "Tag3"]
-        );
-    }
-
-    #endregion
-
     #region When
 
     [When("a POST request is sent to the /Nodes endpoint")]
     public async Task WhenAPostRequestIsSentToTheNodesEndpoint()
     {
-        (_persistedNodeId, _response) = await _apiApiClient.CreateNodeAsync(new CreateNodeRequest(_nodeDto));
+        var (response, rawResponse) =
+            await _apiApiClient.CreateNodeAsync(new SdkCreateNodeRequest
+            {
+                Node = new SdkNodeDto
+                {
+                    Id = null,
+                    Title = "Test Node",
+                    Description = "Test Node Description.",
+                    Timestamp = DateTime.Parse("2024-01-15T09:00:00.000000Z", null, DateTimeStyles.RoundtripKind),
+                    Importance = 1,
+                    Phase = "Testing",
+                    Categories = ["Category1", "Category2"],
+                    Tags = ["Tag1", "Tag2", "Tag3"]
+                }
+            });
+
+        rawResponse.EnsureSuccessStatusCode();
+
+        _persistedNodeId = response!.Id;
+        _response = rawResponse;
 
         _persistedNodeId.Should().NotBeNull();
         _response.EnsureSuccessStatusCode();
@@ -69,16 +67,17 @@ public class NodesApiStepDefinitions
     [Then("the Node is created")]
     public async Task ThenTheNodeIsCreated()
     {
-        var (nodeDto, response) = await _apiApiClient.GetNodeByIdAsync(_persistedNodeId!);
-        
-        response.EnsureSuccessStatusCode();
+        var (response, rawResponse) = await _apiApiClient.GetNodeByIdAsync(_persistedNodeId!);
 
-        nodeDto?.Id.Should().Be(_persistedNodeId?.ToString());
-        nodeDto?.Title.Should().Be(_nodeDto!.Title);
-        nodeDto?.Description.Should().Be(_nodeDto!.Description);
-        nodeDto?.Timestamp.Should().Be(_nodeDto!.Timestamp);
-        nodeDto?.Importance.Should().Be(_nodeDto!.Importance);
-        nodeDto?.Phase.Should().Be(_nodeDto!.Phase);
+        rawResponse.EnsureSuccessStatusCode();
+
+        var nodeDto = response!.NodeDto;
+
+        nodeDto?.Title.Should().Be("Test Node");
+        nodeDto?.Description.Should().Be("Test Node Description.");
+        nodeDto?.Timestamp.Should().Be(DateTime.Parse("2024-01-15T09:00:00.000000Z", null, DateTimeStyles.RoundtripKind));
+        nodeDto?.Importance.Should().Be(1);
+        nodeDto?.Phase.Should().Be("Testing");
     }
 
     #endregion
