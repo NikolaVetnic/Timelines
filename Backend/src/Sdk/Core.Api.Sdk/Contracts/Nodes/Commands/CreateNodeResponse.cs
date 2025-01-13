ca188fba127ab44981aa27167d564c8ca37cdd24
NikolaVetnic <nikola.pacek.vetnic@gmail.com>
2025-01-13 08:24:36 +0100

    feat: create parallel sdk vos, dtos, queries and commands

---
+++ Backend/src/Sdk/Core.Api.Sdk/Contracts/Nodes/Commands/CreateNodeResponse.cs
@@ -0,0 +1,11 @@
+// ReSharper disable UnusedAutoPropertyAccessor.Global
+// // ReSharper disable ClassNeverInstantiated.Global
+
+using Core.Api.Sdk.Contracts.Nodes.ValueObjects;
+
+namespace Core.Api.Sdk.Contracts.Nodes.Commands;
+
+public class CreateNodeResponse
+{
+    public required NodeId Id { get; init; }
+}
