d69cb84283f88f7da47f3720f0b3ea02af31b62e
Vladimir Vuckovic <vidrak97@gmail.com>
2025-01-13 15:09:15 +0100

    chore: make timeline and timelinecard more usable

--- Frontend/react-app/src/components/TimelineCard/TimelineCard.jsx
+++ Frontend/react-app/src/components/TimelineCard/TimelineCard.jsx
@@ -1,27 +1,68 @@
-import React, { useState } from "react";
+import React, { forwardRef, useState } from "react";
 import "./TimelineCard.css";
 
-const TimelineCard = ({ item }) => {
+import Categories from "../Categories/Categories";
+import Description from "../Description/Description";
+import EditableTitle from "../EditableTitle/EditableTitle";
+import Importance from "../Importance/Importance";
+import Tags from "../Tags/Tags";
+import Timestamp from "../Timestamp/Timestamp";
+
+const TimelineCard = forwardRef(({ item, onToggle }, ref) => {
     const [isOpen, setIsOpen] = useState(false);
+    const [categories, setCategories] = useState(item.categories);
+    const [tags, setTags] = useState(item.tags);
+    const [description, setDescription] = useState(item.description);
+    const [timestamp, setTimestamp] = useState(item.timestamp);
+    const [title, setTitle] = useState(item.title);
 
     const toggleCard = () => {
         setIsOpen(!isOpen);
+        onToggle();
     };
 
     return (
-        <div className={`timeline-card ${isOpen ? "open" : ""}`}>
+        <div className={`timeline-card ${isOpen ? "open" : ""}`} ref={ref}>
             <div className="card-header" onClick={toggleCard}>
-                <h3>{item.title}</h3>
+                <EditableTitle
+                    title={title}
+                    onUpdateTitle={(newTitle) => setTitle(newTitle)}
+                    className="timeline-title"
+                />
                 <span>{isOpen ? "-" : "+"}</span>
             </div>
             {isOpen && (
                 <div className="card-content">
-                    <p>{item.description}</p>
-                    {item.image && <img src={item.image} alt={item.title} />}
+                    <Description
+                        description={description}
+                        onUpdateDescription={setDescription}
+                    />
+                    <Timestamp
+                        initialValue={timestamp}
+                        onSave={(newTimestamp) => setTimestamp(newTimestamp)}
+                    />
+                    <Importance
+                        initialValue={item.importance}
+                        onSave={(newImportance) =>
+                            console.log("Saved Importance:", newImportance)
+                        }
+                    />
+                    <p>
+                        <strong>Phase:</strong> {item.phase}
+                    </p>
+                    <Categories
+                        categories={categories}
+                        onUpdateCategories={setCategories}
+                    />
+                    <Tags
+                        tags={tags}
+                        onUpdateTags={setTags}
+                    />
+                    {item.image && <img src={item.image} alt={title} />}
                 </div>
             )}
         </div>
     );
-};
+});
 
 export default TimelineCard;
