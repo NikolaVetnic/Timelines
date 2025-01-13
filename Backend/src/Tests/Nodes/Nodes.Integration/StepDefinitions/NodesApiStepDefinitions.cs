dbe12ed58ef217773c177bfa5cef3d9fa3cc5047
Nikola Vetnić <62119280+NikolaVetnic@users.noreply.github.com>
2025-01-13 12:03:23 +0100

    Introduce Integration Tests to the Solution (#44)

---
+++ Backend/src/Tests/Nodes/Nodes.Integration/StepDefinitions/NodesApiStepDefinitions.cs
@@ -0,0 +1,80 @@
+using SdkNodeId = Core.Api.Sdk.Contracts.Nodes.ValueObjects.NodeId;
+using SdkNodeDto = Core.Api.Sdk.Contracts.Nodes.Dtos.NodeDto;
+using SdkCreateNodeRequest = Core.Api.Sdk.Contracts.Nodes.Commands.CreateNodeRequest;
+
+// ReSharper disable ConvertToPrimaryConstructor
+// ReSharper disable InconsistentNaming
+// ReSharper disable NullableWarningSuppressionIsUsed
+// ReSharper disable Reqnroll.MethodNameMismatchPattern
+
+namespace Nodes.Integration.StepDefinitions;
+
+[Binding]
+public class NodesApiStepDefinitions
+{
+    private readonly ICoreApiClient _apiApiClient;
+    private HttpResponseMessage? _response;
+
+    private SdkNodeDto? _nodeDto;
+    private SdkNodeId? _persistedNodeId;
+
+    public NodesApiStepDefinitions(ICoreApiClient apiClient)
+    {
+        _apiApiClient = apiClient;
+    }
+
+    #region When
+
+    [When("a POST request is sent to the /Nodes endpoint with a valid payload")]
+    public async Task WhenAPostRequestIsSentToTheNodesEndpoint()
+    {
+        var (response, rawResponse) =
+            await _apiApiClient.CreateNodeAsync(new SdkCreateNodeRequest
+            {
+                Node = new SdkNodeDto
+                {
+                    Id = null,
+                    Title = "Test Node",
+                    Description = "Test Node Description.",
+                    Timestamp = DateTime.Parse("2024-01-15T09:00:00.000000Z", null, DateTimeStyles.RoundtripKind),
+                    Importance = 1,
+                    Phase = "Testing",
+                    Categories = ["Category1", "Category2"],
+                    Tags = ["Tag1", "Tag2", "Tag3"]
+                }
+            });
+
+        (_response = rawResponse).EnsureSuccessStatusCode();
+        rawResponse.EnsureSuccessStatusCode();
+
+        (_persistedNodeId = response!.Id).Should().NotBeNull();
+    }
+
+    #endregion
+
+    #region Then
+
+    [Then(@"the response status code is (\d{3}) \((.+)\)")]
+    public void ThenTheResponseStatusCodeIs(int expectedStatusCode, string description)
+    {
+        if (_response != null)
+            ((int)_response.StatusCode).Should().Be(expectedStatusCode);
+    }
+
+    [Then("the Node is created")]
+    public async Task ThenTheNodeIsCreated()
+    {
+        var (response, rawResponse) = await _apiApiClient.GetNodeByIdAsync(_persistedNodeId!);
+
+        rawResponse.EnsureSuccessStatusCode();
+        var nodeDto = response!.NodeDto;
+
+        nodeDto?.Title.Should().Be("Test Node");
+        nodeDto?.Description.Should().Be("Test Node Description.");
+        nodeDto?.Timestamp.Should().Be(DateTime.Parse("2024-01-15T09:00:00.000000Z", null, DateTimeStyles.RoundtripKind));
+        nodeDto?.Importance.Should().Be(1);
+        nodeDto?.Phase.Should().Be("Testing");
+    }
+
+    #endregion
+}
