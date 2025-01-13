200d0aaece3b9859bf1b2e121475f8ed82ffe3f3
NikolaVetnic <nikola.pacek.vetnic@gmail.com>
2025-01-13 08:25:42 +0100

    feat: implement mapping between api and sdk types in client

--- Backend/src/Sdk/Core.Api.Sdk/CoreApiClient.cs
+++ Backend/src/Sdk/Core.Api.Sdk/CoreApiClient.cs
@@ -1,23 +1,49 @@
 using System.Net.Http.Json;
 using Core.Api.Sdk.Interfaces;
+using Mapster;
+using ApiCreateNodeRequest = Nodes.Api.Endpoints.Nodes.CreateNodeRequest;
+using ApiCreateNodeResponse = Nodes.Api.Endpoints.Nodes.CreateNodeResponse;
+using ApiGetNodeByIdResponse = Nodes.Api.Endpoints.Nodes.GetNodeByIdResponse;
+using SdkNodeId = Core.Api.Sdk.Contracts.Nodes.ValueObjects.NodeId;
+using SdkCreateNodeRequest = Core.Api.Sdk.Contracts.Nodes.Commands.CreateNodeRequest;
+using SdkCreateNodeResponse = Core.Api.Sdk.Contracts.Nodes.Commands.CreateNodeResponse;
+using SdkGetNodeByIdResponse = Core.Api.Sdk.Contracts.Nodes.Queries.GetNodeByIdResponse;
 
 namespace Core.Api.Sdk;
 
 public class CoreApiClient(HttpClient httpClient) : ICoreApiClient
 {
-    public async Task<(NodeId? NodeId, HttpResponseMessage RawResponse)> CreateNodeAsync(CreateNodeRequest request)
+    public async Task<(SdkCreateNodeResponse? Response, HttpResponseMessage RawResponse)> CreateNodeAsync(SdkCreateNodeRequest request)
     {
-        var response = await httpClient.PostAsJsonAsync("/Nodes", request);
-        var nodeId = await response.Content.ReadFromJsonAsync<NodeId>();
-        
-        return (nodeId, response);
+        if (request == null)
+            throw new ArgumentNullException(nameof(request));
+
+        var apiRequest = request.Adapt<ApiCreateNodeRequest>();
+        var response = await httpClient.PostAsJsonAsync("/nodes", apiRequest);
+
+        if (!response.IsSuccessStatusCode)
+            return (null, response);
+
+        var apiResponse = await response.Content.ReadFromJsonAsync<ApiCreateNodeResponse>();
+        var sdkCreateNodeResponse = apiResponse.Adapt<SdkCreateNodeResponse>();
+
+        return (sdkCreateNodeResponse, response);
     }
 
-    public async Task<(NodeDto? NodeDto, HttpResponseMessage RawResponse)> GetNodeByIdAsync(NodeId nodeId)
+    public async Task<(SdkGetNodeByIdResponse? Response, HttpResponseMessage RawResponse)> GetNodeByIdAsync(SdkNodeId nodeId)
     {
-        var response = await httpClient.GetAsync($"/Nodes/{nodeId}");
-        var result = await response.Content.ReadFromJsonAsync<GetNodeByIdResponse>();
-        
-        return (result?.NodeDto, response);
+        if (nodeId == null)
+            throw new ArgumentNullException(nameof(nodeId));
+
+        var response = await httpClient.GetAsync($"/nodes/{nodeId.Value}");
+
+        if (!response.IsSuccessStatusCode)
+            return (null, response);
+
+        var apiNode = await response.Content.ReadFromJsonAsync<ApiGetNodeByIdResponse>();
+        var sdkNode = apiNode.Adapt<SdkGetNodeByIdResponse>();
+
+        return (sdkNode, response);
+
     }
 }
