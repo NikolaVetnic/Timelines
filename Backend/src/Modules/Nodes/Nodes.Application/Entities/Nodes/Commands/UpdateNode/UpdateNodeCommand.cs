86a75e525f92a8fbb842adbb7dd8928d302d98d4
Hunor Tot-Bagi <hunortotbagi@gmail.com>
2025-01-14 11:42:34 +0100

    fix: finish up validation for `NodeCommand`

--- Backend/src/Modules/Nodes/Nodes.Application/Entities/Nodes/Commands/UpdateNode/UpdateNodeCommand.cs
+++ Backend/src/Modules/Nodes/Nodes.Application/Entities/Nodes/Commands/UpdateNode/UpdateNodeCommand.cs
@@ -25,23 +25,19 @@ public class UpdateNodeCommandValidator : AbstractValidator<UpdateNodeCommand>
         RuleFor(x => x.Id)
             .NotEmpty().WithMessage("Id is required.");
 
-        // RuleFor(x => x.Title)
-        //     .NotEmpty().When(x => x.Title is not null).WithMessage("Title is required.")
-        //     .MaximumLength(100).WithMessage("Title must not exceed 100 characters.");
-        
-        // RuleFor(x => x.Description)
-        //     .NotEmpty().When(x => x.Title is not null).WithMessage("Description is required.")
-        //     .MaximumLength(500).WithMessage("Description must not exceed 500 characters.");
-        
-        // RuleFor(x => x.Timestamp)
-        //     .LessThanOrEqualTo(DateTime.Now).When(x => x.Timestamp is not null)
-        //     .WithMessage("Timestamp cannot be in the future.");
-        
-        // RuleFor(x => x.Importance)
-        //     .InclusiveBetween(1, 10).When(x => x.Importance is not null)
-        //     .WithMessage("Importance must be between 1 and 10.");
-        
-        // RuleFor(x => x.Phase)
-        //     .NotEmpty().When(x => x.Phase is not null).WithMessage("Phase is required.");
+        RuleFor(x => x.Title)
+            .NotEmpty().When(x => x.Title is not null).WithMessage("Title is required.")
+            .MaximumLength(100).WithMessage("Title must not exceed 100 characters.");
+
+        RuleFor(x => x.Description)
+            .MaximumLength(500).WithMessage("Description must not exceed 500 characters.");
+
+        RuleFor(x => x.Timestamp)
+            .LessThanOrEqualTo(DateTime.Now).When(x => x.Timestamp is not null)
+            .WithMessage("Timestamp cannot be in the future.");
+
+        RuleFor(x => x.Importance)
+            .InclusiveBetween(0, 10).When(x => x.Importance is not null)
+            .WithMessage("Importance must be between 1 and 10.");
     }
 }
