089924006a13e9259de864bc902fa5a4ff44995f
Hunor Tot-Bagi <hunortotbagi@gmail.com>
2025-01-14 13:30:47 +0100

    feat: add miration that changes size property to string

--- Backend/src/Modules/Files/Files.Infrastructure/Data/Migrations/20250114122056_ChangeSizeToString.Designer.cs
+++ Backend/src/Modules/Files/Files.Infrastructure/Data/Migrations/20250114122056_ChangeSizeToString.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Files.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Files.Infrastructure.Data.Migrations
 {
     [DbContext(typeof(FilesDbContext))]
-    partial class FilesDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250114122056_ChangeSizeToString")]
+    partial class ChangeSizeToString
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Backend/src/Modules/Files/Files.Infrastructure/Data/Migrations/20250114122056_ChangeSizeToString.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Files.Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeSizeToString : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Size",
+                schema: "Files",
+                table: "Files",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(float),
+                oldType: "real");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<float>(
+                name: "Size",
+                schema: "Files",
+                table: "Files",
+                type: "real",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+        }
+    }
+}
