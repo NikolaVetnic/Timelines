123a19e920d575bc29fedaa58d9b75cfdfee920c
Hunor Tot-Bagi <hunortotbagi@gmail.com>
2025-01-16 11:50:18 +0100

    feat: implement `ReminderDto` and commands to sdk

---
+++ Backend/src/Sdk/Core.Api.Sdk/Contracts/Reminders/Commands/CreateReminderResponse.cs
@@ -0,0 +1,11 @@
+﻿// ReSharper disable UnusedAutoPropertyAccessor.Global
+// ReSharper disable ClassNeverInstantiated.Global
+
+using BuildingBlocks.Domain.ValueObjects.Ids;
+
+namespace Core.Api.Sdk.Contracts.Reminders.Commands;
+
+public class CreateReminderResponse
+{
+    public required ReminderId Id { get; init; }
+}
