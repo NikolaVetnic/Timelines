18d187df1c81d46a0924890c1f42c5b1565372b1
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2025-01-16 10:36:46 +0100

    feat: add new request to the interface

--- Backend/src/Sdk/Core.Api.Sdk/Interfaces/ICoreApiClient.cs
+++ Backend/src/Sdk/Core.Api.Sdk/Interfaces/ICoreApiClient.cs
@@ -2,6 +2,8 @@ using SdkNodeId = Core.Api.Sdk.Contracts.Nodes.ValueObjects.NodeId;
 using SdkCreateNodeRequest = Core.Api.Sdk.Contracts.Nodes.Commands.CreateNodeRequest;
 using SdkCreateNodeResponse = Core.Api.Sdk.Contracts.Nodes.Commands.CreateNodeResponse;
 using SdkGetNodeByIdResponse = Core.Api.Sdk.Contracts.Nodes.Queries.GetNodeByIdResponse;
+using SdkCreateNoteRequest = Core.Api.Sdk.Contracts.Notes.Commands.CreateNoteRequest;
+using SdkCreateNoteResponse = Core.Api.Sdk.Contracts.Notes.Commands.CreateNoteResponse;
 
 namespace Core.Api.Sdk.Interfaces;
 
@@ -9,4 +11,5 @@ public interface ICoreApiClient
 {
     Task<(SdkCreateNodeResponse? Response, HttpResponseMessage RawResponse)> CreateNodeAsync(SdkCreateNodeRequest request);
     Task<(SdkGetNodeByIdResponse? Response, HttpResponseMessage RawResponse)> GetNodeByIdAsync(SdkNodeId nodeId);
+    Task<(SdkCreateNoteResponse? Response, HttpResponseMessage RawResponse)> CreateNoteAsync(SdkCreateNoteRequest request);
 }
