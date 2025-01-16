cd80d1b7fdf705959184308a477e410511b32cf5
Hunor Tot-Bagi <hunortotbagi@gmail.com>
2025-01-16 12:28:29 +0100

    feat: implement quieries and value objects

---
+++ Backend/src/Sdk/Core.Api.Sdk/Contracts/Reminders/Queries/GetReminderByIdResponse.cs
@@ -0,0 +1,11 @@
+﻿// ReSharper disable UnusedAutoPropertyAccessor.Global
+// ReSharper disable ClassNeverInstantiated.Global
+
+using Core.Api.Sdk.Contracts.Reminders.Dtos;
+
+namespace Core.Api.Sdk.Contracts.Reminders.Queries;
+
+public class GetReminderByIdResponse
+{
+    public required ReminderDto ReminderDto { get; init; }
+}
