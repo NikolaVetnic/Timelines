00a1602cb41f0325352ed1917cdd93d12a6e8f17
Vladimir Vuckovic <vidrak97@gmail.com>
2025-01-28 10:35:50 +0100

    chore: fix error when strip is not properly calculated

--- Frontend/react-app/src/core/utils/RecalculateStrip.jsx
+++ Frontend/react-app/src/core/utils/RecalculateStrip.jsx
@@ -2,8 +2,11 @@ const recalculateStrip = (nodesRef) => {
     if (nodesRef.current.length > 0) {
         const firstNode = nodesRef.current[0];
         const lastNode = nodesRef.current[nodesRef.current.length - 1];
-        const topOffset = firstNode.offsetTop + firstNode.offsetHeight / 1.5;
-        const bottomOffset = lastNode.offsetTop + lastNode.offsetHeight / 1.5;
+
+        if (!firstNode || !lastNode) return {};
+
+        const topOffset = firstNode.offsetTop + firstNode.offsetHeight / 2;
+        const bottomOffset = lastNode.offsetTop + lastNode.offsetHeight / 2;
 
         return {
             top: `${topOffset}px`,
