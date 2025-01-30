ca81cce6cbccce5cfccb8359bb0a4d37ed4e0f10
Hunor Tot-Bagi <hunortotbagi@gmail.com>
2025-01-30 14:10:27 +0100

    refactor: extend `Dto` with `BaseDto`

--- Backend/src/BuildingBlocks/BuildingBlocks.Domain/Reminders/Dtos/ReminderDto.cs
+++ Backend/src/BuildingBlocks/BuildingBlocks.Domain/Reminders/Dtos/ReminderDto.cs
@@ -11,21 +11,7 @@ public class ReminderDto(
     int priority,
     DateTime notificationTime,
     string status,
-    NodeBaseDto node) // todo: Refactor this so that it extends ReminderBaseDto
+    NodeBaseDto node) : ReminderBaseDto(id, title, description, dueDateTime, priority, notificationTime, status)
 {
-    [JsonPropertyName("id")] public string? Id { get; } = id;
-
-    [JsonPropertyName("title")] public string Title { get; } = title;
-
-    [JsonPropertyName("description")] public string Description { get; } = description;
-
-    [JsonPropertyName("dueDateTime")] public DateTime DueDateTime { get; } = dueDateTime;
-
-    [JsonPropertyName("priority")] public int Priority { get; } = priority;
-
-    [JsonPropertyName("notificationTime")] public DateTime NotificationTime { get; } = notificationTime;
-
-    [JsonPropertyName("status")] public string Status { get; } = status;
-    
     [JsonPropertyName("node")] public NodeBaseDto Node { get; set; } = node;
 }
