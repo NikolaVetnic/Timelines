13de0b200effcb7ce997c6d9885ef7652209992f
Hunor Tot-Bagi <hunortotbagi@gmail.com>
2025-02-03 10:16:39 +0100

    feat: add timelines repositories

---
+++ Backend/src/Modules/Timelines/Timelines.Application/Data/Abstractions/ITimelinesRepository.cs
@@ -0,0 +1,9 @@
+﻿using BuildingBlocks.Domain.Timelines.Timeline.ValueObjects;
+
+namespace Timelines.Application.Data.Abstractions;
+
+public interface ITimelinesRepository
+{
+    Task<Timeline> GetTimelineByIdAsync(TimelineId timelineId, CancellationToken cancellationToken);
+    Task UpdateTimelineAsync(Timeline timeline, CancellationToken cancellationToken);
+}
---
+++ Backend/src/Modules/Timelines/Timelines.Infrastructure/Data/Repositories/TimelineRepository.cs
@@ -0,0 +1,22 @@
+﻿using BuildingBlocks.Domain.Timelines.Timeline.ValueObjects;
+using Timelines.Application.Data.Abstractions;
+using Timelines.Application.Entities.Timelines.Exceptions;
+
+namespace Timelines.Infrastructure.Data.Repositories;
+
+public class TimelineRepository(ITimelinesDbContext dbContext) : ITimelinesRepository
+{
+    public async Task<Timeline> GetTimelineByIdAsync(TimelineId timelineId, CancellationToken cancellationToken)
+    {
+        return await dbContext.Timelines
+                   .AsNoTracking()
+                   .SingleOrDefaultAsync(t => t.Id == timelineId, cancellationToken) ??
+               throw new TimelineNotFoundException(timelineId.ToString());
+    }
+
+    public async Task UpdateTimelineAsync(Timeline timeline, CancellationToken cancellationToken)
+    {
+        dbContext.Timelines.Update(timeline);
+        await dbContext.SaveChangesAsync(cancellationToken);
+    }
+}
