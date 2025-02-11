5f80c6f9af677efa7ac2f3bde8073c87c296160f
Hunor Tot-Bagi <hunortotbagi@gmail.com>
2025-02-11 19:35:19 +0100

    feat: move events to `BuildingBlocks` & fix up the issues

---
+++ Backend/src/BuildingBlocks/BuildingBlocks.Domain/Notes/Note/Events/NoteCreatedEvent.cs
@@ -0,0 +1,7 @@
+﻿using BuildingBlocks.Domain.Abstractions;
+using BuildingBlocks.Domain.ValueObjects.Ids;
+
+namespace BuildingBlocks.Domain.Notes.Note.Events
+{
+    public record NoteCreatedEvent(NoteId NoteId) : IDomainEvent;
+}
