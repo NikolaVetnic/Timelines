43820893480ccba88f67c64e3ebf43e84659ae0c
Hunor Tot-Bagi <hunortotbagi@gmail.com>
2025-02-12 07:53:27 +0100

    feat: implement `NotesRepository` and register it

---
+++ Backend/src/Modules/Notes/Notes.Infrastructure/Data/Repositories/NotesRepository.cs
@@ -0,0 +1,22 @@
+﻿using BuildingBlocks.Domain.Notes.Note.ValueObjects;
+using Notes.Application.Data.Abstractions;
+using Notes.Application.Entities.Notes.Exceptions;
+
+namespace Notes.Infrastructure.Data.Repositories;
+
+public class NotesRepository(INotesDbContext dbContext) : INotesRepository
+{
+    public async Task<Note> GetNoteByIdAsync(NoteId noteId, CancellationToken cancellationToken)
+    {
+        return await dbContext.Notes
+                   .AsNoTracking()
+                   .SingleOrDefaultAsync(n => n.Id == noteId, cancellationToken) ??
+               throw new NoteNotFoundException(noteId.ToString());
+    }
+
+    public async Task UpdateNoteAsync(Note note, CancellationToken cancellationToken)
+    {
+        dbContext.Notes.Update(note);
+        await dbContext.SaveChangesAsync(cancellationToken);
+    }
+}
