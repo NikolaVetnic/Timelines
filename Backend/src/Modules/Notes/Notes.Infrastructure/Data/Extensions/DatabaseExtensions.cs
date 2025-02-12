a3726ea8ed69b2418419c2775f1ae4cc5eb4cd79
Hunor Tot-Bagi <hunortotbagi@gmail.com>
2025-02-12 12:51:23 +0100

    feat: existing `NoteId`s will be added to connected `Node` when seeding database

--- Backend/src/Modules/Notes/Notes.Infrastructure/Data/Extensions/DatabaseExtensions.cs
+++ Backend/src/Modules/Notes/Notes.Infrastructure/Data/Extensions/DatabaseExtensions.cs
@@ -1,4 +1,7 @@
-﻿namespace Notes.Infrastructure.Data.Extensions;
+﻿using BuildingBlocks.Application.Data;
+using Notes.Application.Data;
+
+namespace Notes.Infrastructure.Data.Extensions;
 
 public static class DatabaseExtensions
 {
@@ -7,21 +10,25 @@ public static class DatabaseExtensions
         using var scope = services.CreateScope();
         var scopedProvider = scope.ServiceProvider;
 
-        var context = scopedProvider.GetRequiredService<NotesDbContext>();
+        var notesDbContext = scopedProvider.GetRequiredService<NotesDbContext>();
+        var nodesService = scopedProvider.GetRequiredService<INodesService>();
 
         // Apply migrations
-        await context.Database.MigrateAsync();
+        await notesDbContext.Database.MigrateAsync();
 
         // Seed initial data if necessary
-        await SeedAsync(context);
+        await SeedAsync(notesDbContext, nodesService);
     }
 
-    private static async Task SeedAsync(NotesDbContext context)
+    private static async Task SeedAsync(NotesDbContext context, INodesService nodesService)
     {
         if (await context.Notes.AnyAsync())
             return;
 
         await context.AddRangeAsync(InitialData.Notes);
         await context.SaveChangesAsync();
+
+        foreach (var initialNote in InitialData.Notes)
+            await nodesService.AddNote(initialNote.NodeId, initialNote.Id, CancellationToken.None);
     }
 }
