82693289a020a512415aed450f7da704427c1b9a
Hunor Tot-Bagi <hunortotbagi@gmail.com>
2025-03-07 09:55:43 +0100

    feat: register file service

--- Backend/src/Modules/Files/Files.Api/Extensions/ServiceCollectionExtensions.cs
+++ Backend/src/Modules/Files/Files.Api/Extensions/ServiceCollectionExtensions.cs
@@ -1,4 +1,5 @@
 using BuildingBlocks.Api.Converters;
+using BuildingBlocks.Application.Data;
 using Files.Application.Extensions;
 using Files.Infrastructure.Data.Extensions;
 using Microsoft.Extensions.Configuration;
@@ -21,6 +22,8 @@ public static class ServiceCollectionExtensions
     private static IServiceCollection AddApiServices(this IServiceCollection services)
     {
         TypeAdapterConfig.GlobalSettings.Scan(typeof(FileIdConverter).Assembly);
+
+        services.AddScoped<IFileAssetsService, IFileAssetsService>();
 
         return services;
     }
