4ec19a756dd6aff6506170718285ec58c091d56c
Hunor Tot-Bagi <hunortotbagi@gmail.com>
2025-03-13 12:13:57 +0100

    feat: register repository

--- Backend/src/Modules/Files/Files.Infrastructure/Data/Extensions/ServiceCollectionExtensions.cs
+++ Backend/src/Modules/Files/Files.Infrastructure/Data/Extensions/ServiceCollectionExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using Files.Application.Data.Abstractions;
+using Files.Infrastructure.Data.Repositories;
 using Microsoft.EntityFrameworkCore.Diagnostics;
 using Microsoft.Extensions.Configuration;
 
@@ -20,6 +21,7 @@ public static class ServiceCollectionExtensions
 
         // Register DbContext interface
         services.AddScoped<IFilesDbContext, FilesDbContext>();
+        services.AddScoped<IFilesRepository, FileAssetsRepository>();
 
         return services;
     }
