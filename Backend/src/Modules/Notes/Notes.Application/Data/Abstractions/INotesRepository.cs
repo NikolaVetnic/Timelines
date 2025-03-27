a8a68bb7cad1ea1f07fb75439179da90fc85d51c
Hunor Tot-Bagi <hunortotbagi@gmail.com>
2025-03-27 10:42:38 +0100

    refactor: enchance `NotesRepository`

--- Backend/src/Modules/Notes/Notes.Application/Data/Abstractions/INotesRepository.cs
+++ Backend/src/Modules/Notes/Notes.Application/Data/Abstractions/INotesRepository.cs
@@ -4,6 +4,12 @@ namespace Notes.Application.Data.Abstractions;
 
 public interface INotesRepository
 {
+    Task<List<Note>> ListNotesPaginatedAsync(int pageIndex, int pageSize, CancellationToken cancellationToken);
+    Task<long> NoteCountAsync(CancellationToken cancellationToken);
+
     Task<Note> GetNoteByIdAsync(NoteId noteId, CancellationToken cancellationToken);
+    Task<List<Note>> GetNotesByIdsAsync(IEnumerable<NoteId> noteIds, CancellationToken cancellationToken);
+
     Task UpdateNoteAsync(Note note, CancellationToken cancellationToken);
+    Task RemoveNote(Note note, CancellationToken cancellationToken);
 }
--- Backend/src/Modules/Notes/Notes.Infrastructure/Data/Repositories/NotesRepository.cs
+++ Backend/src/Modules/Notes/Notes.Infrastructure/Data/Repositories/NotesRepository.cs
@@ -6,6 +6,24 @@ namespace Notes.Infrastructure.Data.Repositories;
 
 public class NotesRepository(INotesDbContext dbContext) : INotesRepository
 {
+    #region List
+    public async Task<List<Note>> ListNotesPaginatedAsync(int pageIndex, int pageSize, CancellationToken cancellationToken)
+    {
+        return await dbContext.Notes
+            .AsNoTracking()
+            .OrderBy(n => n.Timestamp)
+            .Skip(pageSize * pageIndex)
+            .Take(pageSize)
+            .ToListAsync(cancellationToken: cancellationToken);
+    }
+
+    public async Task<long> NoteCountAsync(CancellationToken cancellationToken)
+    {
+        return await dbContext.Notes.LongCountAsync(cancellationToken);
+    }
+    #endregion
+
+    #region Get
     public async Task<Note> GetNoteByIdAsync(NoteId noteId, CancellationToken cancellationToken)
     {
         return await dbContext.Notes
@@ -14,9 +32,24 @@ public class NotesRepository(INotesDbContext dbContext) : INotesRepository
                throw new NoteNotFoundException(noteId.ToString());
     }
 
+    public async Task<List<Note>> GetNotesByIdsAsync(IEnumerable<NoteId> noteIds, CancellationToken cancellationToken)
+    {
+        return await dbContext.Notes
+            .AsNoTracking()
+            .Where(n => noteIds.Contains(n.Id))
+            .ToListAsync(cancellationToken);
+    }
+    #endregion
+
     public async Task UpdateNoteAsync(Note note, CancellationToken cancellationToken)
     {
         dbContext.Notes.Update(note);
+        await dbContext.SaveChangesAsync(cancellationToken);
+    }
+
+    public async Task RemoveNote(Note note, CancellationToken cancellationToken)
+    {
+        dbContext.Notes.Remove(note);
         await dbContext.SaveChangesAsync(cancellationToken);
     }
 }
