b3f77e0fd948d5875f279e904fc2ba06cb78df6d
stamenione <vidrak97@gmail.com>
2025-04-03 12:00:28 +0200

    chore: introduce routing and refactor app.jsx

--- Frontend/react-app/src/App.jsx
+++ Frontend/react-app/src/App.jsx
@@ -1,37 +1,23 @@
-import React, { useState } from "react";
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router";
 import Timeline from "./components/Timelines/Timeline/Timeline";
-import TimelineSelect from "./components/Timelines/TimelineSelect/TimelineSelect";
-import CreateTimelineModal from "./core/components/modals/CreateTimelineModal/CreateTimelineModal";
-import timelineData from "./data/timelineData";
+import TimelineList from "./components/Timelines/TimelineList/TimelineList";
 import "./styles/App.css";
 
 function App() {
-    const [selectedTimeline, setSelectedTimeline] = useState(null);
-    const [isModalOpen, setIsModalOpen] = useState(false);
-
-    const handleTimelineSelect = (selectedOption) => {
-        const foundTimeline = timelineData.find(timeline => timeline.id === selectedOption.value);
-        setSelectedTimeline(foundTimeline ? { ...foundTimeline, nodes: [...foundTimeline.nodes] } : null);
-    };
-
-    const handleOpenModal = () => {
-        setIsModalOpen(true);
-    };
-
-    const handleCloseModal = () => {
-        setIsModalOpen(false);
-    };
-
     return (
-        <div className="app-container">
-            <div className="app-content">
-                <TimelineSelect onTimelineSelect={handleTimelineSelect} />
-                <button onClick={handleOpenModal}>Create Timeline</button>
-                <Timeline selectedTimeline={selectedTimeline} />
+        <BrowserRouter>
+            <div className="app-container">
+                <div className="app-content">
+                    <div className="content-wrapper">
+                    <Routes>
+                        <Route path="/timelines/:id" element={<Timeline />} />
+                        <Route path="/" element={<TimelineList />} />
+                    </Routes>
+                    </div>
+                </div>
             </div>
-
-            {isModalOpen && <CreateTimelineModal onClose={handleCloseModal} />}
-        </div>
+        </BrowserRouter>
     );
 }
 
