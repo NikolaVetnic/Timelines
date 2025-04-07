95e0b2b562154e1ccfbadc9e05ce91becb332b6d
stamenione <vidrak97@gmail.com>
2025-04-07 15:38:02 +0200

    chore: improve create nod modal and form field

--- Frontend/react-app/src/core/components/forms/FormField/FormField.jsx
+++ Frontend/react-app/src/core/components/forms/FormField/FormField.jsx
@@ -1,12 +1,44 @@
-const FormField = ({ label, type, name, value, onChange, placeholder, min, max }) => (
-    <div className="form-field">
-        <label>{label}:</label>
-        {type === "textarea" ? (
-            <textarea name={name} value={value} onChange={onChange} placeholder={placeholder} />
-        ) : (
-            <input type={type} name={name} value={value} onChange={onChange} placeholder={placeholder} min={min} max={max} />
-        )}
-    </div>
+import "./FormField.css";
+
+const FormField = ({
+  label,
+  type,
+  name,
+  value,
+  onChange,
+  placeholder,
+  min,
+  max,
+  required,
+  error,
+}) => (
+  <div className="form-field">
+    <label>
+      {label}
+      {required && <span className="required-asterisk">*</span>}
+    </label>
+    {type === "textarea" ? (
+      <textarea
+        name={name}
+        value={value}
+        onChange={onChange}
+        placeholder={placeholder}
+        className={error ? "error-input" : ""}
+      />
+    ) : (
+      <input
+        type={type}
+        name={name}
+        value={value}
+        onChange={onChange}
+        placeholder={placeholder}
+        min={min}
+        max={max}
+        className={error ? "error-input" : ""}
+      />
+    )}
+    {error && <div className="form-field-error">{error}</div>}
+  </div>
 );
 
 export default FormField;
