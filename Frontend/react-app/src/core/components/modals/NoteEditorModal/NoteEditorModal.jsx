d4fb96f895f10b4fc060d99d986e91912ebfa472
stamenione <vidrak97@gmail.com>
2025-04-14 15:36:48 +0200

    chore: adapt UI of note editor

--- Frontend/react-app/src/core/components/modals/NoteEditorModal/NoteEditorModal.jsx
+++ Frontend/react-app/src/core/components/modals/NoteEditorModal/NoteEditorModal.jsx
@@ -1,40 +1,67 @@
 import MDEditor from "@uiw/react-md-editor";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom";
-
+import { FaTimes } from "react-icons/fa";
 import Button from "../../buttons/Button/Button";
 import "./NoteEditorModal.css";
 
-const NoteEditor = ({ selectedNote, editorContent, setEditorContent, handleSaveNote, closeNoteEditor }) => {
-  const root = "note-modal";
+const NoteEditor = ({ 
+  selectedNote, 
+  editorContent, 
+  setEditorContent, 
+  handleSaveNote, 
+  closeNoteEditor 
+}) => {
+  const root = "note-editor-modal";
+  const [showModal, setShowModal] = useState(false);
+
+  useEffect(() => {
+    if (selectedNote) {
+      requestAnimationFrame(() => {
+        setShowModal(true);
+      });
+    }
+  }, [selectedNote]);
 
   if (!selectedNote) return null;
 
   return ReactDOM.createPortal(
-    <div className={`${root}-overlay`}>
-      <div className={`${root}-overlay-content`}>
-        <div className={`${root}-header`}>
-          <h2>{selectedNote.title}</h2>
+    <div className={`${root}-overlay ${showModal ? 'show' : ''}`}>
+      <div className={`${root}-content ${showModal ? 'show' : ''}`}>
+        <div className={`${root}-close`}>
+          <Button
+            icon={<FaTimes />}
+            iconOnly
+            noBackground
+            variant="danger"
+            onClick={closeNoteEditor}
+          />
         </div>
         
-        <div className="editor-container">
+        <div className={`${root}-header`}>
+          <h3>{selectedNote.title}</h3>
+        </div>
+        
+        <div className={`${root}-editor`}>
           <MDEditor
             value={editorContent}
             onChange={(val) => setEditorContent(val?.toString() || "")}
             height="100%"
           />
         </div>
-        <div className={`${root}-buttons`}>
-            <Button
-              text="Cancel"
-              onClick={closeNoteEditor}
-            />
-            <Button 
-              text="Save"
-              variant="success"
-              onClick={handleSaveNote}
-            />
-          </div>
+        
+        <div className={`${root}-actions`}>
+          <Button
+            text="Cancel"
+            variant="secondary"
+            onClick={closeNoteEditor}
+          />
+          <Button 
+            text="Save"
+            variant="success"
+            onClick={handleSaveNote}
+          />
+        </div>
       </div>
     </div>,
     document.body
