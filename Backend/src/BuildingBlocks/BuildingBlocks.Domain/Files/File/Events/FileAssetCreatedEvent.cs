3dcc84abe6d48f33a24611b6f716caafc3bc00f8
Hunor Tot-Bagi <hunortotbagi@gmail.com>
2025-04-17 10:15:50 +0200

    feat: move events to `BuildingBlocks`

---
+++ Backend/src/BuildingBlocks/BuildingBlocks.Domain/Files/File/Events/FileAssetCreatedEvent.cs
@@ -0,0 +1,6 @@
+﻿using BuildingBlocks.Domain.Abstractions;
+using BuildingBlocks.Domain.ValueObjects.Ids;
+
+namespace BuildingBlocks.Domain.Files.File.Events;
+
+public record FileAssetCreatedEvent(FileAssetId FileAssetId) : IDomainEvent;
