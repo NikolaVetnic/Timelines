8d5b40169cf9e3a27c3d7edde26d8e666d507b39
Hunor Tot-Bagi <hunortotbagi@gmail.com>
2025-04-24 21:58:02 +0200

    refactor: hard code database seeding for `Nodes` & `Reminders`

--- Backend/src/Modules/Nodes/Nodes.Infrastructure/Data/Extensions/InitialData.cs
+++ Backend/src/Modules/Nodes/Nodes.Infrastructure/Data/Extensions/InitialData.cs
@@ -1,5 +1,6 @@
 using BuildingBlocks.Domain.Files.File.ValueObjects;
 using BuildingBlocks.Domain.Nodes.Node.ValueObjects;
+using BuildingBlocks.Domain.Reminders.Reminder.ValueObjects;
 using BuildingBlocks.Domain.Timelines.Timeline.ValueObjects;
 
 namespace Nodes.Infrastructure.Data.Extensions;
@@ -20,7 +21,8 @@ internal static class InitialData
                 Categories = ["court", "complaint"],
                 Tags = ["investigation", "interview"],
                 TimelineId = TimelineId.Of(Guid.Parse("d87c1c54-b287-4a82-b45b-65db1d71e2fd")),
-                FileAssetIds = [FileAssetId.Of(Guid.Parse("d79293f2-4910-44e2-bfbb-690a6f24f703"))]
+                FileAssetIds = [FileAssetId.Of(Guid.Parse("d79293f2-4910-44e2-bfbb-690a6f24f703"))],
+                ReminderIds = [ReminderId.Of(Guid.Parse("2e1c4902-7841-484c-b997-0a8cd3955e72"))]
             },
 
             new()
@@ -34,7 +36,8 @@ internal static class InitialData
                 Categories = ["court", "hearing"],
                 Tags = ["witness", "vital"],
                 TimelineId = TimelineId.Of(Guid.Parse("f739b2c7-883e-46c6-917c-d29114e3e017")),
-                FileAssetIds = [FileAssetId.Of(Guid.Parse("16d56e5f-dcea-4b1f-82e3-4c0fdb142773"))]
+                FileAssetIds = [FileAssetId.Of(Guid.Parse("16d56e5f-dcea-4b1f-82e3-4c0fdb142773"))],
+                ReminderIds = [ReminderId.Of(Guid.Parse("74f40a78-bda2-4177-bffd-86e6648c4318"))]
             }
         };
 }
