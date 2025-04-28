0c91f57f206e52c88117c6086088c4943304d50c
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2025-04-28 09:39:33 +0200

    feat: add new property

--- Backend/src/Modules/Users/Users.Application/Entities/Authentication/Commands/RegisterUser/RegisterUserCommand.cs
+++ Backend/src/Modules/Users/Users.Application/Entities/Authentication/Commands/RegisterUser/RegisterUserCommand.cs
@@ -3,4 +3,6 @@
 namespace Users.Application.Entities.Authentication.Commands.RegisterUser;
 public record RegisterUserCommand : ICommand
 {
+    public required string Email { get; set; }
+    public required string Password { get; set; }
 }
