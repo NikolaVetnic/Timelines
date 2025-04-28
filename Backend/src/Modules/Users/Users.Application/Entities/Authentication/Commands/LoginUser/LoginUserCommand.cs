e1f9f5f4fe4b696b4f68fd9d6fd096e128071813
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2025-04-28 09:38:53 +0200

    feat: add login command/response

---
+++ Backend/src/Modules/Users/Users.Application/Entities/Authentication/Commands/LoginUser/LoginUserCommand.cs
@@ -0,0 +1,14 @@
+﻿using BuildingBlocks.Application.Cqrs;
+
+namespace Users.Application.Entities.Authentication.Commands.LoginUser;
+public record LoginUserCommand : ICommand
+{
+    public required string Email { get; set; }
+    public required string Password { get; set; }
+}
+
+public record AuthenticationResponse
+{
+    public bool Success { get; set; }
+    public string Error { get; set; }
+}
