e7b8652064f916667ba9fe6b96d980988b19ea1a
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2025-04-28 09:39:11 +0200

    feat: implement login handler

---
+++ Backend/src/Modules/Users/Users.Application/Entities/Authentication/Commands/LoginUser/LoginUserHandler.cs
@@ -0,0 +1,33 @@
+﻿using BuildingBlocks.Application.Cqrs;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+using Users.Domain.Models;
+
+namespace Users.Application.Entities.Authentication.Commands.LoginUser;
+internal class LoginUserHandler(UserManager<ApplicationUser> userManager) : ICommandHandler<LoginUserCommand, AuthenticationResponse>
+{
+    private readonly UserManager<ApplicationUser> _userManager = userManager;
+
+    public async Task<AuthenticationResponse> Handle(LoginUserCommand command, CancellationToken cancellationToken)
+    {
+        var user = await _userManager.FindByEmailAsync(command.Email);
+
+        if (user == null)
+            return new AuthenticationResponse { Success = false, Error = "User not found" };
+
+        var passwordValid = await _userManager.CheckPasswordAsync(user, command.Password);
+
+        if (!passwordValid)
+            return new AuthenticationResponse { Success = false, Error = "Invalid password" };
+
+        var token = await GenerateJwtToken(user);
+
+        return new AuthenticationResponse
+        {
+            Success = true,
+            //Token = token,
+            //UserId = user.Id,
+            //Email = user.Email
+        };
+    }
+}
