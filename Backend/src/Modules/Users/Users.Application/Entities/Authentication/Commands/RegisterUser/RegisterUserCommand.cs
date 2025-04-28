d4772973d623b80da859bfc2fb52ad8de2d4b34a
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2025-04-28 07:42:10 +0200

    feat: add register user handler

---
+++ Backend/src/Modules/Users/Users.Application/Entities/Authentication/Commands/RegisterUser/RegisterUserCommand.cs
@@ -0,0 +1,6 @@
+﻿using BuildingBlocks.Application.Cqrs;
+
+namespace Users.Application.Entities.Authentication.Commands.RegisterUser;
+public record RegisterUserCommand : ICommand
+{
+}
---
+++ Backend/src/Modules/Users/Users.Application/Entities/Authentication/Commands/RegisterUser/RegisterUserHandler.cs
@@ -0,0 +1,30 @@
+﻿using BuildingBlocks.Application.Cqrs;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+using Users.Domain.Models;
+
+namespace Users.Application.Entities.Authentication.Commands.RegisterUser;
+internal class RegisterUserHandler : ICommandHandler<RegisterUserCommand>
+{
+    public async Task<Unit> Handle(RegisterUserCommand request, CancellationToken cancellationToken)
+    {
+        var user = new ApplicationUser
+        {
+            Email = request.Email,
+            UserName = request.Email
+        };
+
+        var result = await _userManager.CreateAsync(user, request.Password);
+
+        if (!result.Succeeded)
+        {
+            var errors = result.Errors.Select(e => e.Description);
+            return new AuthResponse { Success = false, Errors = errors };
+        }
+
+        // Optionally add default role
+        await _userManager.AddToRoleAsync(user, "User");
+
+        return new AuthResponse { Success = true };
+    }
+}
