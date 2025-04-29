8a6d82849dc4a6053a27bad2d538970773771418
NikolaVetnic <nikola.pacek.vetnic@gmail.com>
2025-04-29 11:56:56 +0200

    feat: add update file asset functionality

---
+++ Backend/src/Modules/Files/Files.Api/Endpoints/Files/UpdateFileAsset.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections.Generic;
+using BuildingBlocks.Domain.Files.File.Dtos;
+using BuildingBlocks.Domain.Files.File.ValueObjects;
+using BuildingBlocks.Domain.Nodes.Node.ValueObjects;
+using Files.Application.Entities.Files.Commands.UpdateFileAsset;
+
+namespace Files.Api.Endpoints.Files;
+
+public class UpdateFileAsset : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapPut("/Files/{fileId}", async (string fileId, CreateFileAssetRequest request, ISender sender) =>
+            {
+                var command = new UpdateFileAssetCommand
+                {
+                    Id = FileAssetId.Of(Guid.Parse(fileId)),
+                    Name = request.Name,
+                    Description = request.Description,
+                    SharedWith = request.SharedWith,
+                    IsPublic = request.IsPublic,
+                    NodeId = request.NodeId
+                };
+                
+                var result = await sender.Send(command);
+                var response = result.Adapt<UpdateFileAssetResponse>();
+                
+                return Results.Ok(response);
+            })
+            .WithName("UpdateFileAsset")
+            .Produces<UpdateFileAssetResponse>()
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .WithSummary("Update File Asset")
+            .WithDescription("Updates a file asset");
+    }
+}
+
+// ReSharper disable once NotAccessedPositionalProperty.Global
+public record UpdateFileAssetRequest
+{
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public List<string> SharedWith { get; set; }
+    public bool IsPublic { get; set; }
+    public NodeId NodeId { get; set; }
+}
+
+// ReSharper disable once ClassNeverInstantiated.Global
+// ReSharper disable once NotAccessedPositionalProperty.Global
+public record UpdateFileAssetResponse(FileAssetDto FileAssetDto);
---
+++ Backend/src/Modules/Files/Files.Application/Entities/Files/Commands/UpdateFileAsset/UpdateFileAssetHandler.cs
@@ -0,0 +1,44 @@
+﻿using BuildingBlocks.Application.Data;
+using BuildingBlocks.Domain.Nodes.Node.ValueObjects;
+using Files.Application.Data.Abstractions;
+using Files.Application.Entities.Files.Exceptions;
+using Files.Application.Entities.Files.Extensions;
+
+namespace Files.Application.Entities.Files.Commands.UpdateFileAsset;
+
+internal class UpdateFileAssetHandler(IFilesRepository filesRepository, INodesService nodesService)
+    : ICommandHandler<UpdateFileAssetCommand, UpdateFileAssetResult>
+{
+    public async Task<UpdateFileAssetResult> Handle(UpdateFileAssetCommand command, CancellationToken cancellationToken)
+    {
+        var fileAsset =
+            await filesRepository.GetFileAssetByIdAsync(command.Id, cancellationToken);
+
+        if (fileAsset == null)
+            throw new FileAssetNotFoundException(command.Id.ToString());
+
+        fileAsset.Name = command.Name ?? fileAsset.Name;
+        fileAsset.Description = command.Description ?? fileAsset.Description;
+        fileAsset.SharedWith = command.SharedWith ?? fileAsset.SharedWith;
+        fileAsset.IsPublic = command.IsPublic ?? fileAsset.IsPublic;
+
+        var node = await nodesService.GetNodeByIdAsync(fileAsset.NodeId, cancellationToken);
+
+        if (command.NodeId != null)
+        {
+            node = await nodesService.GetNodeByIdAsync(command.NodeId, cancellationToken);
+
+            if (node == null)
+                throw new NotFoundException(command.NodeId.ToString());
+        }
+
+        if (node.Id == null)
+            throw new NotFoundException($"Related with id {command.NodeId} not found");
+
+        fileAsset.NodeId = NodeId.Of(Guid.Parse(node.Id));
+
+        await filesRepository.UpdateFileAssetAsync(fileAsset, cancellationToken);
+
+        return new UpdateFileAssetResult(fileAsset.ToFileAssetDto(node));
+    }
+}
