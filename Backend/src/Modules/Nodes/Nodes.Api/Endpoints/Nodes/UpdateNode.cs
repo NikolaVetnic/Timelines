499c7a2320174259e67eff39bcad2f9fd5492922
Nikola Vetnić <62119280+NikolaVetnic@users.noreply.github.com>
2025-04-29 15:47:04 +0200

    Introduce Update Functionalities for All Entities (#116)

---
+++ Backend/src/Modules/Nodes/Nodes.Api/Endpoints/Nodes/UpdateNode.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections.Generic;
+using BuildingBlocks.Domain.Nodes.Node.Dtos;
+using BuildingBlocks.Domain.Nodes.Node.ValueObjects;
+using BuildingBlocks.Domain.Timelines.Timeline.ValueObjects;
+using Nodes.Application.Entities.Nodes.Commands.UpdateNode;
+
+// ReSharper disable ClassNeverInstantiated.Global
+// ReSharper disable NotAccessedPositionalProperty.Global
+// ReSharper disable UnusedAutoPropertyAccessor.Global
+
+namespace Nodes.Api.Endpoints.Nodes;
+
+public class UpdateNode : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapPut("/Nodes/{nodeId}", async (string nodeId, UpdateNodeRequest request, ISender sender) =>
+            {
+                var command = new UpdateNodeCommand
+                {
+                    Id = NodeId.Of(Guid.Parse(nodeId)),
+                    Title = request.Title,
+                    Description = request.Description,
+                    Phase = request.Phase,
+                    Timestamp = request.Timestamp,
+                    Importance = request.Importance,
+                    Categories = request.Categories,
+                    Tags = request.Tags,
+                    TimelineId = request.TimelineId
+                };
+                
+                var result = await sender.Send(command);
+                var response = result.Adapt<UpdateNodeResponse>();
+
+                return Results.Ok(response);
+            })
+        .WithName("UpdateNode")
+        .Produces<UpdateNodeResponse>()
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .WithSummary("Update Node")
+        .WithDescription("Updates a node");
+    }
+}
+
+public class UpdateNodeRequest
+{
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public DateTime Timestamp { get; set; }
+    public int Importance { get; set; }
+    public string Phase { get; set; }
+    public List<string> Categories { get; set; }
+    public List<string> Tags { get; set; }
+    public TimelineId TimelineId { get; set; }
+}
+
+public record UpdateNodeResponse(NodeBaseDto Node);
---
+++ Backend/src/Modules/Notes/Notes.Application/Entities/Notes/Commands/UpdateNote/UpdateNoteHandler.cs
@@ -0,0 +1,36 @@
+﻿using BuildingBlocks.Application.Data;
+using BuildingBlocks.Domain.Nodes.Node.ValueObjects;
+using Notes.Application.Data.Abstractions;
+using Notes.Application.Entities.Notes.Exceptions;
+using Notes.Application.Entities.Notes.Extensions;
+
+namespace Notes.Application.Entities.Notes.Commands.UpdateNote;
+
+internal class UpdateNoteHandler(INotesRepository notesRepository, INodesService nodesService)
+    : ICommandHandler<UpdateNoteCommand, UpdateNoteResult>
+{
+    public async Task<UpdateNoteResult> Handle(UpdateNoteCommand command, CancellationToken cancellationToken)
+    {
+        var note = await notesRepository.GetNoteByIdAsync(command.Id, cancellationToken);
+
+        if (note is null)
+            throw new NoteNotFoundException(command.Id.ToString());
+
+        note.Title = command.Title ?? note.Title;
+        note.Content = command.Content ?? note.Content;
+        note.Timestamp = command.Timestamp ?? note.Timestamp;
+        note.SharedWith = command.SharedWith ?? note.SharedWith;
+        note.IsPublic = command.IsPublic ?? note.IsPublic;
+
+        var node = await nodesService.GetNodeByIdAsync(command.NodeId ?? note.NodeId, cancellationToken);
+
+        if (node.Id is null)
+            throw new NotFoundException($"Related node with ID {command.NodeId ?? note.NodeId} not found");
+
+        note.NodeId = NodeId.Of(Guid.Parse(node.Id));
+
+        await notesRepository.UpdateNoteAsync(note, cancellationToken);
+
+        return new UpdateNoteResult(note.ToNoteDto(node));
+    }
+}
