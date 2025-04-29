2f0b99c1d66214786ecbaaee29de0dc7b5cb466c
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2025-04-29 18:10:18 +0200

    feat: add private method, GenerateJwtToken()

--- Backend/src/Modules/Users/Users.Application/Entities/Authentication/Commands/LoginUser/LoginUserHandler.cs
+++ Backend/src/Modules/Users/Users.Application/Entities/Authentication/Commands/LoginUser/LoginUserHandler.cs
@@ -1,12 +1,17 @@
-﻿using BuildingBlocks.Application.Cqrs;
-using MediatR;
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using BuildingBlocks.Application.Cqrs;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 using Users.Domain.Models;
 
 namespace Users.Application.Entities.Authentication.Commands.LoginUser;
-internal class LoginUserHandler(UserManager<ApplicationUser> userManager) : ICommandHandler<LoginUserCommand, AuthenticationResponse>
+internal class LoginUserHandler(UserManager<ApplicationUser> userManager, IConfiguration configuration) : ICommandHandler<LoginUserCommand, AuthenticationResponse>
 {
     private readonly UserManager<ApplicationUser> _userManager = userManager;
+    private readonly IConfiguration _configuration = configuration;
 
     public async Task<AuthenticationResponse> Handle(LoginUserCommand command, CancellationToken cancellationToken)
     {
@@ -29,5 +34,32 @@ internal class LoginUserHandler(UserManager<ApplicationUser> userManager) : ICom
             //UserId = user.Id,
             //Email = user.Email
         };
+    }
+
+    private async Task<string> GenerateJwtToken(ApplicationUser user)
+    {
+        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]));
+        var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+        var roles = await _userManager.GetRolesAsync(user);
+
+        var claims = new List<Claim>
+        {
+            new Claim(JwtRegisteredClaimNames.Sub, user.Id),
+            new Claim(JwtRegisteredClaimNames.Email, user.Email),
+            new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+        };
+
+        claims.AddRange(roles.Select(role => new Claim(ClaimTypes.Role, role)));
+
+        var token = new JwtSecurityToken(
+            issuer: _configuration["Jwt:Issuer"],
+            audience: _configuration["Jwt:Audience"],
+            claims: claims,
+            expires: DateTime.Now.AddHours(Convert.ToDouble(_configuration["Jwt:ExpireHours"])),
+            signingCredentials: credentials
+        );
+
+        return new JwtSecurityTokenHandler().WriteToken(token);
     }
 }
