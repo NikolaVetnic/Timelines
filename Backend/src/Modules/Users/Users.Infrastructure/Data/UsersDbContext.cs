26af8681a01b24a643f49660fca58d78ad31d9cc
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2025-04-30 14:41:34 +0200

    feat: implement db context

--- Backend/src/Modules/Users/Users.Infrastructure/Data/UsersDbContext.cs
+++ Backend/src/Modules/Users/Users.Infrastructure/Data/UsersDbContext.cs
@@ -1,9 +1,31 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using System.Reflection;
 using Users.Application.Data.Abstractions;
 using Users.Domain.Models;
 
 namespace Users.Infrastructure.Data;
 
 public class UsersDbContext(DbContextOptions<UsersDbContext> options)
-    : IdentityDbContext<ApplicationUser>(options), IUsersDbContext;
+    : IdentityDbContext<ApplicationUser>(options), IUsersDbContext
+{
+    public DbSet<ApplicationUser> ApplicationUsers { get; init; }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+
+        // Specify schema for this module
+        builder.HasDefaultSchema("Users");
+
+        // Configure entities
+        builder.Entity<ApplicationUser>(entity =>
+        {
+            entity.ToTable("Users"); // Specify table name within the schema
+        });
+
+        // Apply all configurations taken from classes that implement IEntityTypeConfiguration<>
+        builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        base.OnModelCreating(builder);
+    }
+}
