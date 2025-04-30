3f37b2ab9c5c4dcfe6e2a0d3e62372b0aa9f1bf9
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2025-04-30 13:45:28 +0200

    feat: add user configuration

---
+++ Backend/src/Modules/Users/Users.Infrastructure/Data/Configurations/UserConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Users.Domain.Models;
+
+namespace Users.Infrastructure.Data.Configurations;
+
+public class UserConfiguration : IEntityTypeConfiguration<ApplicationUser>
+{
+    public void Configure(EntityTypeBuilder<ApplicationUser> builder)
+    {
+        builder.HasKey(u => u.Id);
+
+        builder.Property(u => u.Email);
+        builder.Property(u => u.PasswordHash);
+    }
+}
