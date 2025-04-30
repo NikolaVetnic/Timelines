eaa4ed36df2fe1b1df033f3447814033dbdd2450
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2025-04-30 12:59:39 +0200

    feat: add user id converter

---
+++ Backend/src/BuildingBlocks/BuildingBlocks.Api/Converters/UserIdConverter.cs
@@ -0,0 +1,9 @@
+﻿using BuildingBlocks.Domain.Users.User.ValueObjects;
+
+namespace BuildingBlocks.Api.Converters;
+
+public class UserIdConverter
+{
+    public void Register(TypeAdapterConfig config) =>
+        config.NewConfig<UserId, UserId>().ConstructUsing(src => UserId.Of(src.Value));
+}
