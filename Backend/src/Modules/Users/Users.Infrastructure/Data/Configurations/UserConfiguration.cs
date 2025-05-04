3a522cda0bb3e020ae2df55322ce819eb7054693
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2025-05-04 20:32:26 +0200

    feat: implement new configuration

--- Backend/src/Modules/Users/Users.Infrastructure/Data/Configurations/UserConfiguration.cs
+++ Backend/src/Modules/Users/Users.Infrastructure/Data/Configurations/UserConfiguration.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using BuildingBlocks.Domain.Users.User.ValueObjects;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using Users.Domain.Models;
 
@@ -9,8 +10,13 @@ public class UserConfiguration : IEntityTypeConfiguration<ApplicationUser>
     public void Configure(EntityTypeBuilder<ApplicationUser> builder)
     {
         builder.HasKey(u => u.Id);
+        builder.Property(u => u.Id).HasConversion(
+            id => id.Value,
+            userId => UserId.Of(userId));
 
-        builder.Property(u => u.Email);
-        builder.Property(u => u.PasswordHash);
+        builder.Property(u => u.Email)
+            .IsRequired();
+        builder.Property(u => u.PasswordHash)
+            .IsRequired();
     }
 }
