b4babad3867c70a4b2a0025c7ce68c6a91229ba9
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2025-05-04 20:31:51 +0200

    feat: add new ApplicationRole

--- Backend/src/Modules/Users/Users.Domain/Models/ApplicationUser.cs
+++ Backend/src/Modules/Users/Users.Domain/Models/ApplicationUser.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using BuildingBlocks.Domain.Users.User.ValueObjects;
+using Microsoft.AspNetCore.Identity;
 
 namespace Users.Domain.Models;
 
-public class ApplicationUser : IdentityUser;
+public class ApplicationUser : IdentityUser<UserId>;
+
+public class ApplicationRole : IdentityRole<UserId>;
