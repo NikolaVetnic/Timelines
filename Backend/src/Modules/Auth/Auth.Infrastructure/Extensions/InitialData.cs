1389fb9d6518ad3a924a36831d6f4034e17e1722
NikolaVetnic <nikola.pacek.vetnic@gmail.com>
2025-05-08 00:35:20 +0200

    feat: add USRa and USRb seed data

---
+++ Backend/src/Modules/Auth/Auth.Infrastructure/Extensions/InitialData.cs
@@ -0,0 +1,34 @@
+﻿namespace Auth.Infrastructure.Extensions;
+
+internal static class InitialData
+{
+    public static IEnumerable<SeededUser> SeededUsers =>
+        new List<SeededUser>
+        {
+            new()
+            {
+                Id = "11111111-1111-1111-1111-111111111111",
+                Username = "USRa",
+                Email = "USRa@email.com",
+                FullName = "User A",
+                Password = "P@ssw0rd!123"
+            },
+            new()
+            {
+                Id = "22222222-2222-2222-2222-222222222222",
+                Username = "USRb",
+                Email = "USRb@email.com",
+                FullName = "User B",
+                Password = "P@ssw0rd?456"
+            }
+        };
+}
+
+public class SeededUser
+{
+    public required string Id { get; init; }
+    public required string Username { get; init; }
+    public required string Email { get; init; }
+    public required string FullName { get; init; }
+    public required string Password { get; init; }
+}
