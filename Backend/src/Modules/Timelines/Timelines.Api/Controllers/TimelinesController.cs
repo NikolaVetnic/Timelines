31ea1a44906fe48f7c129d3a30a8016a77a4dacc
NikolaVetnic <nikola.pacek.vetnic@gmail.com>
2025-05-08 01:14:05 +0200

    feat: add create timeline with template endpoint to classic controller

--- Backend/src/Modules/Timelines/Timelines.Api/Controllers/TimelinesController.cs
+++ Backend/src/Modules/Timelines/Timelines.Api/Controllers/TimelinesController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Threading.Tasks;
 using BuildingBlocks.Application.Pagination;
 using BuildingBlocks.Domain.Timelines.Timeline.Dtos;
@@ -6,6 +7,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using OpenIddict.Validation.AspNetCore;
 using Timelines.Application.Entities.Timelines.Commands.CreateTimeline;
+using Timelines.Application.Entities.Timelines.Commands.CreateTimelineWithTemplate;
 using Timelines.Application.Entities.Timelines.Queries.GetTimelineById;
 using Timelines.Application.Entities.Timelines.Queries.ListTimelines;
 
@@ -21,12 +23,12 @@ public class TimelinesController(ISender sender) : ControllerBase
     [ProducesResponseType(StatusCodes.Status400BadRequest)]
     public async Task<ActionResult<ListTimelinesResponse>> Get([FromQuery] PaginationRequest query)
     {
-        var result   = await sender.Send(new ListTimelinesQuery(query));
+        var result = await sender.Send(new ListTimelinesQuery(query));
         var response = result.Adapt<ListTimelinesResponse>();
-        
+
         return Ok(response);
     }
-    
+
     [HttpGet("{timelineId}")]
     [ProducesResponseType(typeof(GetTimelineByIdResponse), StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status400BadRequest)]
@@ -35,14 +37,14 @@ public class TimelinesController(ISender sender) : ControllerBase
         [FromRoute] string timelineId)
     {
         var result = await sender.Send(new GetTimelineByIdQuery(timelineId));
-            
+
         if (result is null)
             return NotFound();
 
         var response = result.Adapt<GetTimelineByIdResponse>();
         return Ok(response);
     }
-    
+
     [HttpPost]
     [ProducesResponseType(typeof(CreateTimelineResponse), StatusCodes.Status201Created)]
     [ProducesResponseType(StatusCodes.Status400BadRequest)]
@@ -58,6 +60,28 @@ public class TimelinesController(ISender sender) : ControllerBase
             new { timelineId = response.Id },
             response);
     }
+
+    [HttpPost("Clone/{timelineId}")]
+    [ProducesResponseType(typeof(CreateTimelineWithTemplateResponse), StatusCodes.Status201Created)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public async Task<ActionResult<CreateTimelineWithTemplateResponse>> Clone(
+        [FromRoute] string timelineId,
+        [FromBody] CreateTimelineWithTemplateRequest request)
+    {
+        var command = new CreateTimelineWithTemplateCommand
+        {
+            Id = TimelineId.Of(Guid.Parse(timelineId)),
+            Title = request.Title,
+            Description = request.Description
+        };
+        var result = await sender.Send(command);
+        var response = result.Adapt<CreateTimelineWithTemplateResponse>();
+
+        return CreatedAtAction(
+            nameof(GetById),
+            new { timelineId = response.Timeline.Id },
+            response);
+    }
 }
 
 public record ListTimelinesResponse(PaginatedResult<TimelineDto> Timelines);
@@ -71,3 +95,11 @@ public class CreateTimelineRequest
 }
 
 public record CreateTimelineResponse(TimelineId Id);
+
+public class CreateTimelineWithTemplateRequest
+{
+    public string Title { get; set; }
+    public string Description { get; set; }
+}
+
+public record CreateTimelineWithTemplateResponse(TimelineBaseDto Timeline);
