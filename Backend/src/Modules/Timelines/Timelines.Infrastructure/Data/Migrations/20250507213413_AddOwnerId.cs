c558a9eeb60c4e3168cd76dc354431fb4ab072d4
NikolaVetnic <nikola.pacek.vetnic@gmail.com>
2025-05-08 00:38:46 +0200

    feat: add timeline owner id

---
+++ Backend/src/Modules/Timelines/Timelines.Infrastructure/Data/Migrations/20250507213413_AddOwnerId.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Timelines.Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddOwnerId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "OwnerId",
+                schema: "Timelines",
+                table: "Timelines",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "OwnerId",
+                schema: "Timelines",
+                table: "Timelines");
+        }
+    }
+}
