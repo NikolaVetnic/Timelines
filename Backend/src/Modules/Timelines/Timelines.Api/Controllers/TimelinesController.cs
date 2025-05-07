417938e4f8ea94619a8f640dd5a9f891e50aae80
NikolaVetnic <nikola.pacek.vetnic@gmail.com>
2025-05-08 01:03:26 +0200

    feat: add get timeline by id endpoint to classic controller

--- Backend/src/Modules/Timelines/Timelines.Api/Controllers/TimelinesController.cs
+++ Backend/src/Modules/Timelines/Timelines.Api/Controllers/TimelinesController.cs
@@ -4,6 +4,7 @@ using BuildingBlocks.Domain.Timelines.Timeline.Dtos;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using OpenIddict.Validation.AspNetCore;
+using Timelines.Application.Entities.Timelines.Queries.GetTimelineById;
 using Timelines.Application.Entities.Timelines.Queries.ListTimelines;
 
 namespace Timelines.Api.Controllers;
@@ -23,7 +24,23 @@ public class TimelinesController(ISender sender) : ControllerBase
         
         return Ok(response);
     }
+    
+    [HttpGet("{timelineId}")]
+    [ProducesResponseType(typeof(GetTimelineByIdResponse), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<ActionResult<GetTimelineByIdResponse>> GetById(
+        [FromRoute] string timelineId)
+    {
+        var result = await sender.Send(new GetTimelineByIdQuery(timelineId));
+            
+        if (result is null)
+            return NotFound();
+
+        var response = result.Adapt<GetTimelineByIdResponse>();
+        return Ok(response);
+    }
 }
 
-// you can keep this in its own file if you prefer
 public record ListTimelinesResponse(PaginatedResult<TimelineDto> Timelines);
+public record GetTimelineByIdResponse(TimelineDto Timeline);
