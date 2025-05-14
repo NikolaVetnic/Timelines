31237c9919b648a3e9b8fdb8be100b790acf67e8
stamenione <vidrak97@gmail.com>
2025-05-14 11:27:26 +0200

    fix: add missing imports

--- Frontend/react-app/src/core/components/lists/TimelineList/TimelineList.jsx
+++ Frontend/react-app/src/core/components/lists/TimelineList/TimelineList.jsx
@@ -1,5 +1,6 @@
 import { useCallback, useEffect, useRef, useState } from "react";
-import { FaTrash } from "react-icons/fa";
+import { CiEdit } from "react-icons/ci";
+import { FaSignOutAlt, FaTrash } from "react-icons/fa";
 import { PiSelectionAll, PiSelectionAllFill } from "react-icons/pi";
 import { useNavigate } from "react-router";
 import { useAuth } from "../../../../context/AuthContext";
