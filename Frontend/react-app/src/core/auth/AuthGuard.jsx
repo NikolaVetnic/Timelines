a17b207277f5025223a6b710f1c300f7735449a2
stamenione <vidrak97@gmail.com>
2025-05-15 12:20:53 +0200

    feat: adapt auth guard to support page loading and save the location state

--- Frontend/react-app/src/core/auth/AuthGuard.jsx
+++ Frontend/react-app/src/core/auth/AuthGuard.jsx
@@ -1,18 +1,31 @@
-import { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
 import { useAuth } from "../../context/AuthContext";
 
 const AuthGuard = ({ children }) => {
-  const { isAuthenticated } = useAuth();
+  const { isAuthenticated, isLoading } = useAuth();
   const navigate = useNavigate();
+  const location = useLocation();
+  const [initialCheckDone, setInitialCheckDone] = useState(false);
 
   useEffect(() => {
-    if (!isAuthenticated()) {
-      navigate("/login");
+    if (!isLoading) {
+      if (!isAuthenticated()) {
+        navigate("/login", { 
+          replace: true,
+          state: { from: location } 
+        });
+      } else {
+        setInitialCheckDone(true);
+      }
     }
-  }, [isAuthenticated, navigate]);
+  }, [isAuthenticated, isLoading, navigate, location]);
 
-  return isAuthenticated() ? children : null;
+  if (isLoading || !initialCheckDone) {
+    return <div>Loading...</div>;
+  }
+
+  return children;
 };
 
 export default AuthGuard;
