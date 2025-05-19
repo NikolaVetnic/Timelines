c007ec2770d1042cb92aa4a0608871f5f5edc921
stamenione <vidrak97@gmail.com>
2025-05-19 08:30:05 +0200

    refactor: extract AppLayout to be a separate component

--- Frontend/react-app/src/App.jsx
+++ Frontend/react-app/src/App.jsx
@@ -1,55 +1,19 @@
-import { useState } from "react";
-import { FaBug } from "react-icons/fa";
 import {
   createBrowserRouter,
   createRoutesFromElements,
-  Outlet,
   Route,
   RouterProvider,
 } from "react-router-dom";
-import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import AppLayout from "./components/AppLayout/AppLayout";
 import ErrorPage from "./components/Pages/ErrorPage";
 import LoginPage from "./components/Pages/LoginPage";
 import Timeline from "./components/Timelines/Timeline/Timeline";
 import { AuthProvider } from "./context/AuthContext";
 import AuthGuard from "./core/auth/AuthGuard";
 import TimelineList from "./core/components/lists/TimelineList/TimelineList";
-import BugReportModal from "./core/components/modals/BugReportModal/BugReportModal";
-import ReminderNotifier from "./core/utils/ReminderNotifier";
 import "./styles/App.css";
 
-function AppLayout() {
-  const [isBugReportOpen, setIsBugReportOpen] = useState(false);
-
-  return (
-    <div className="app-container">
-      <ReminderNotifier />
-      <div className="app-content">
-        <div className="content-wrapper">
-          <Outlet />
-          <ToastContainer />
-        </div>
-      </div>
-
-      {/* this is a temporary button only for testing an application and reporting bugs to the developers */}
-      <button
-        className="bug-report-button"
-        onClick={() => setIsBugReportOpen(true)}
-        aria-label="Report a bug"
-      >
-        <FaBug size={20} />
-      </button>
-
-      {isBugReportOpen && (
-        <BugReportModal
-          isOpen={isBugReportOpen}
-          onClose={() => setIsBugReportOpen(false)}
-        />
-      )}
-    </div>
-  );
-}
 
 const router = createBrowserRouter(
   createRoutesFromElements(
