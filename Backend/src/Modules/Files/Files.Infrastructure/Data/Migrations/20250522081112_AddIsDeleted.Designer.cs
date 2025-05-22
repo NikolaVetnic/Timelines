47498191868d8472b00ac95efbfd8d7b2cb33549
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2025-05-22 10:18:27 +0200

    feat: add migration, files

--- Backend/src/Modules/Files/Files.Infrastructure/Data/Migrations/20250522081112_AddIsDeleted.Designer.cs
+++ Backend/src/Modules/Files/Files.Infrastructure/Data/Migrations/20250522081112_AddIsDeleted.Designer.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using Files.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -12,9 +13,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Files.Infrastructure.Data.Migrations
 {
     [DbContext(typeof(FilesDbContext))]
-    partial class FilesDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250522081112_AddIsDeleted")]
+    partial class AddIsDeleted
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
