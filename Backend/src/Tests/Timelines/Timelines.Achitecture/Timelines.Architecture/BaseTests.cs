9aaac201b4645b0cf8d6124554165d0327660d0d
Hunor Tot-Bagi <hunortotbagi@gmail.com>
2025-05-25 10:05:15 +0200

    feat: add `Timelines` architecture tests

---
+++ Backend/src/Tests/Timelines/Timelines.Achitecture/Timelines.Architecture/BaseTests.cs
@@ -0,0 +1,14 @@
+﻿using BuildingBlocks.Application.Cqrs;
+using Timelines.Domain.Models;
+using Timelines.Infrastructure.Data;
+using Assembly = System.Reflection.Assembly;
+
+namespace Timelines.Architecture;
+
+public abstract class BaseTests
+{
+    protected static readonly Assembly DomainAssembly = typeof(Timeline).Assembly;
+    protected static readonly Assembly ApplicationAssembly = typeof(ICommand).Assembly;
+    protected static readonly Assembly InfrastructureAssembly = typeof(TimelinesDbContext).Assembly;
+    protected static readonly Assembly ApiAssembly = typeof(Program).Assembly;
+}
