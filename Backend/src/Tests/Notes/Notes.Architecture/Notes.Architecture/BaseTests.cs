0f40f64d5b1d7b4150e1950a585fce8f127d7aa6
Hunor Tot-Bagi <hunortotbagi@gmail.com>
2025-05-25 09:49:08 +0200

    feat: add `Notes` architecture tests

---
+++ Backend/src/Tests/Notes/Notes.Architecture/Notes.Architecture/BaseTests.cs
@@ -0,0 +1,15 @@
+﻿using BuildingBlocks.Application.Cqrs;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+using Notes.Domain.Models;
+using Notes.Infrastructure.Data;
+using Assembly = System.Reflection.Assembly;
+
+namespace Notes.Architecture;
+
+public abstract class BaseTests
+{
+    protected static readonly Assembly DomainAssembly = typeof(Note).Assembly;
+    protected static readonly Assembly ApplicationAssembly = typeof(ICommand).Assembly;
+    protected static readonly Assembly InfrastructureAssembly = typeof(NotesDbContext).Assembly;
+    protected static readonly Assembly ApiAssembly = typeof(Program).Assembly;
+}
