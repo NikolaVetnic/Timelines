b9972f29878e4a96d71c6c24d1cd32ac8e6e730d
Hunor Tot-Bagi <hunortotbagi@gmail.com>
2025-05-25 09:16:48 +0200

    feat: add `Auth` architecture tests

---
+++ Backend/src/Tests/Auth/Auth.Architecture/Auth.Architecture/BaseTests.cs
@@ -0,0 +1,14 @@
+﻿using BuildingBlocks.Application.Cqrs;
+using Auth.Domain.Models;
+using Auth.Infrastructure.Data;
+using Assembly = System.Reflection.Assembly;
+
+namespace Auth.Architecture;
+
+public abstract class BaseTests
+{
+    protected static readonly Assembly DomainAssembly = typeof(ApplicationUser).Assembly;
+    protected static readonly Assembly ApplicationAssembly = typeof(ICommand).Assembly;
+    protected static readonly Assembly InfrastructureAssembly = typeof(AuthDbContext).Assembly;
+    protected static readonly Assembly ApiAssembly = typeof(Program).Assembly;
+}
