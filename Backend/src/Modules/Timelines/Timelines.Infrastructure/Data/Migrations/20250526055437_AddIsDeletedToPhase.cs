d2295a543579a67b6332ca58f6287d6a0e4e0215
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2025-05-26 07:55:14 +0200

    feat: add migration, Phase

---
+++ Backend/src/Modules/Timelines/Timelines.Infrastructure/Data/Migrations/20250526055437_AddIsDeletedToPhase.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Timelines.Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddIsDeletedToPhase : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DeletedAt",
+                schema: "Timelines",
+                table: "Phases",
+                type: "timestamp with time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "DeletedBy",
+                schema: "Timelines",
+                table: "Phases",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                schema: "Timelines",
+                table: "Phases",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DeletedAt",
+                schema: "Timelines",
+                table: "Phases");
+
+            migrationBuilder.DropColumn(
+                name: "DeletedBy",
+                schema: "Timelines",
+                table: "Phases");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                schema: "Timelines",
+                table: "Phases");
+        }
+    }
+}
