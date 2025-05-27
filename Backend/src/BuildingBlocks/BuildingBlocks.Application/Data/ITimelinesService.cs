9c8e077d20aff76618daba282fb05e0633a004de
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2025-05-27 11:31:11 +0200

    feat: add new method to timelines service

--- Backend/src/BuildingBlocks/BuildingBlocks.Application/Data/ITimelinesService.cs
+++ Backend/src/BuildingBlocks/BuildingBlocks.Application/Data/ITimelinesService.cs
@@ -8,6 +8,7 @@ namespace BuildingBlocks.Application.Data;
 public interface ITimelinesService
 {
     Task<List<TimelineDto>> ListTimelinesPaginated(int pageIndex, int pageSize, CancellationToken cancellationToken);
+    Task<List<TimelineDto>> ListFlaggedForDeletionTimelinesPaginated(int pageIndex, int pageSize, CancellationToken cancellationToken);
     Task<TimelineDto> GetTimelineByIdAsync(TimelineId timelineId, CancellationToken cancellationToken);
     Task<TimelineBaseDto> GetTimelineBaseByIdAsync(TimelineId timelineId, CancellationToken cancellationToken);
     Task<long> CountTimelinesAsync(CancellationToken cancellationToken);
--- Backend/src/Modules/Timelines/Timelines.Application/Data/TimelinesService.cs
+++ Backend/src/Modules/Timelines/Timelines.Application/Data/TimelinesService.cs
@@ -69,6 +69,58 @@ public class TimelinesService(IServiceProvider serviceProvider, ITimelinesReposi
             .ToList();
         return timelineDtos;
     }
+
+    public async Task<List<TimelineDto>> ListFlaggedForDeletionTimelinesPaginated(int pageIndex, int pageSize, CancellationToken cancellationToken)
+    {
+
+        var timelines = await timelinesRepository.ListFlaggedForDeletionTimelinesPaginatedAsync(pageIndex, pageSize, cancellationToken);
+
+        var nodes = await NodesService.GetNodesBaseBelongingToTimelineIdsAsync(timelines.Select(t => t.Id),
+            cancellationToken);
+
+        var phases = await PhasesService.GetPhasesBaseBelongingToTimelineIdsAsync(timelines.Select(t => t.Id),
+            cancellationToken);
+
+        var timelineDtos = timelines.Select(t =>
+                t.ToTimelineDto(
+                    nodes
+                        .Where(n => t.NodeIds.Select(id => id.ToString()).Contains(n.Id))
+                        .Select(n => new NodeBaseDto(
+                            id: n.Id!.ToString(),
+                            title: n.Title,
+                            description: n.Description,
+                            timestamp: n.Timestamp,
+                            importance: n.Importance,
+                            categories: n.Categories,
+                            tags: n.Tags)
+                        )
+                        .ToList(),
+                    phases
+                        .Where(p => t.PhaseIds.Select(id => id.ToString()).Contains(p.Id))
+                        .Select(p => new PhaseBaseDto(
+                            id: p.Id!.ToString(),
+                            title: p.Title,
+                            description: p.Description,
+                            startDate: p.StartDate,
+                            endDate: p.EndDate,
+                            duration: p.Duration,
+                            status: p.Status,
+                            progress: p.Progress,
+                            isCompleted: p.IsCompleted,
+                            parent: p.Parent,
+                            dependsOn: p.DependsOn,
+                            assignedTo: p.AssignedTo,
+                            stakeholders: p.Stakeholders,
+                            tags: p.Tags
+                            )
+                        )
+                        .ToList()
+
+                ))
+            .ToList();
+        return timelineDtos;
+    }
+
     public async Task<long> CountTimelinesAsync(CancellationToken cancellationToken)
     {
         return await timelinesRepository.TimelineCountAsync(cancellationToken);
