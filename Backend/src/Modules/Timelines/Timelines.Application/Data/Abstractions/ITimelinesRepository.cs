cc833e032c69bb627ad0d4ede9b19d6773d777d9
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2025-05-27 11:22:05 +0200

    feat: add new method to timelines repository interface

--- Backend/src/Modules/Timelines/Timelines.Application/Data/Abstractions/ITimelinesRepository.cs
+++ Backend/src/Modules/Timelines/Timelines.Application/Data/Abstractions/ITimelinesRepository.cs
@@ -6,6 +6,7 @@ namespace Timelines.Application.Data.Abstractions;
 public interface ITimelinesRepository
 {
     Task<List<Timeline>> ListTimelinesPaginatedAsync(int pageIndex, int pageSize, CancellationToken cancellationToken);
+    Task<List<Timeline>> ListFlaggedForDeletionTimelinesPaginatedAsync(int pageIndex, int pageSize, CancellationToken cancellationToken);
     Task<long> TimelineCountAsync(CancellationToken cancellationToken);
 
     Task<Timeline> GetTimelineByIdAsync(TimelineId timelineId, CancellationToken cancellationToken);
