f729c7f652b235511b82b3763c21dbab49e1cf4e
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2025-05-28 10:25:55 +0200

    feat: add new handler, files feat: add new query, files

---
+++ Backend/src/Modules/Files/Files.Application/Entities/Files/Queries/ListFlaggedForDeletionFileAssets/ListFlaggedForDeletionFileAssetsHandler.cs
@@ -0,0 +1,25 @@
+﻿using BuildingBlocks.Application.Data;
+using BuildingBlocks.Application.Pagination;
+using BuildingBlocks.Domain.Files.File.Dtos;
+
+namespace Files.Application.Entities.Files.Queries.ListFlaggedForDeletionFileAssets;
+
+internal class ListFlaggedForDeletionFileAssetsHandler(IFilesService filesService) : IQueryHandler<ListFlaggedForDeletionFileAssetsQuery, ListFlaggedForDeletionFileAssetsResult>
+{
+    public async Task<ListFlaggedForDeletionFileAssetsResult> Handle(ListFlaggedForDeletionFileAssetsQuery query, CancellationToken cancellationToken)
+    {
+        var pageIndex = query.PaginationRequest.PageIndex;
+        var pageSize = query.PaginationRequest.PageSize;
+
+        var totalCount = await filesService.CountFileAssetsAsync(cancellationToken);
+
+        var fileAssets = await filesService.ListFileAssetsPaginated(pageIndex, pageSize, cancellationToken);
+
+        return new ListFlaggedForDeletionFileAssetsResult(
+            new PaginatedResult<FileAssetDto>(
+                pageIndex,
+                pageSize,
+                totalCount,
+                fileAssets));
+    }
+}
