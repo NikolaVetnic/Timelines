d519fe00cd0cc5f64982eca72c5df142ff8826b9
Hunor Tot-Bagi <hunortotbagi@gmail.com>
2025-05-29 10:46:00 +0200

    fix: use repository in `ListPhasesHandler` & fix performance issue

--- Backend/src/BuildingBlocks/BuildingBlocks.Application/Data/IPhasesService.cs
+++ Backend/src/BuildingBlocks/BuildingBlocks.Application/Data/IPhasesService.cs
@@ -6,13 +6,11 @@ namespace BuildingBlocks.Application.Data;
 
 public interface IPhasesService
 {
-    Task<List<PhaseDto>> ListPhasesPaginated(int pageIndex, int pageSize, CancellationToken cancellationToken);
     Task<PhaseBaseDto> GetPhaseBaseByIdAsync(PhaseId phaseId, CancellationToken cancellationToken);
     Task<PhaseDto> GetPhaseByIdAsync(PhaseId phaseId, CancellationToken cancellationToken);
     Task<List<PhaseBaseDto>> GetPhasesByIdsAsync(IEnumerable<PhaseId> phaseIds, CancellationToken cancellationToken);
     Task<List<PhaseBaseDto>> GetPhasesBaseBelongingToTimelineIdsAsync(IEnumerable<TimelineId> timelineIds, CancellationToken cancellationToken);
 
-    Task<long> CountPhasesAsync(CancellationToken cancellationToken);
 
     Task DeletePhase(PhaseId phaseId, CancellationToken cancellationToken);
     Task DeletePhases(TimelineId timelineId, IEnumerable<PhaseId> phaseIds, CancellationToken cancellationToken);
--- Backend/src/Modules/Timelines/Timelines.Application/Entities/Phases/Queries/ListPhases/ListPhasesHandler.cs
+++ Backend/src/Modules/Timelines/Timelines.Application/Entities/Phases/Queries/ListPhases/ListPhasesHandler.cs
@@ -1,25 +1,30 @@
 ﻿using BuildingBlocks.Application.Data;
 using BuildingBlocks.Application.Pagination;
 using BuildingBlocks.Domain.Timelines.Phase.Dtos;
+using Timelines.Application.Data.Abstractions;
+using Timelines.Application.Entities.Phases.Extensions;
 
 namespace Timelines.Application.Entities.Phases.Queries.ListPhases;
 
-internal class ListPhasesHandler(IPhasesService phasesService) : IQueryHandler<ListPhasesQuery, ListPhasesResult>
+internal class ListPhasesHandler(IPhasesRepository phasesRepository, ITimelinesService timelineService) : IQueryHandler<ListPhasesQuery, ListPhasesResult>
 {
     public async Task<ListPhasesResult> Handle(ListPhasesQuery query, CancellationToken cancellationToken)
     {
         var pageIndex = query.PaginationRequest.PageIndex;
         var pageSize = query.PaginationRequest.PageSize;
 
-        var totalCount = await phasesService.CountPhasesAsync(cancellationToken);
+        var totalCount = await phasesRepository.PhaseCountAsync(cancellationToken);
 
-        var phases = await phasesService.ListPhasesPaginated(pageIndex, pageSize, cancellationToken);
+        var phases = await phasesRepository.ListPhasesPaginatedAsync(pageIndex, pageSize, cancellationToken);
+        var timeline = await timelineService.GetTimelineBaseByIdAsync(phases[0].TimelineId, cancellationToken);
+
+        var phaseDtos = phases.Select(p => p.ToPhaseDto(timeline)).ToList(); 
 
         return new ListPhasesResult(
             new PaginatedResult<PhaseDto>(
                 pageIndex,
                 pageSize,
                 totalCount,
-                phases));
+                phaseDtos));
     }
 }
