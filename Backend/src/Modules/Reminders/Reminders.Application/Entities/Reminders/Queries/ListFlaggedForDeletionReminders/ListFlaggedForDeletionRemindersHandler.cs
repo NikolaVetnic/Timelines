e2f3171c62d314fbf8806496c05f853ac09ee57a
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2025-06-02 11:22:51 +0200

    feat: add new handler, reminders

---
+++ Backend/src/Modules/Reminders/Reminders.Application/Entities/Reminders/Queries/ListFlaggedForDeletionReminders/ListFlaggedForDeletionRemindersHandler.cs
@@ -0,0 +1,25 @@
+﻿using BuildingBlocks.Application.Data;
+using BuildingBlocks.Application.Pagination;
+using BuildingBlocks.Domain.Reminders.Reminder.Dtos;
+
+namespace Reminders.Application.Entities.Reminders.Queries.ListFlaggedForDeletionReminders;
+
+internal class ListFlaggedForDeletionRemindersHandler(IRemindersService remindersService) : IQueryHandler<ListFlaggedForDeletionRemindersQuery, ListFlaggedForDeletionRemindersResult>
+{
+    public async Task<ListFlaggedForDeletionRemindersResult> Handle(ListFlaggedForDeletionRemindersQuery query, CancellationToken cancellationToken)
+    {
+        var pageIndex = query.PaginationRequest.PageIndex;
+        var pageSize = query.PaginationRequest.PageSize;
+
+        var reminders = await remindersService.ListFlaggedForDeletionRemindersPaginated(pageIndex, pageSize, cancellationToken);
+
+        var totalCount = reminders.Count;
+
+        return new ListFlaggedForDeletionRemindersResult(
+            new PaginatedResult<ReminderDto>(
+                pageIndex,
+                pageSize,
+                totalCount,
+                reminders));
+    }
+}
