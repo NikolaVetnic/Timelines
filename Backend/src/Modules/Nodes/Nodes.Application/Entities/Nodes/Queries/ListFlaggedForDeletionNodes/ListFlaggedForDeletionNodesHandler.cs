57b4c919e8e7ae42d68a3eec3fb8d64995a3c865
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2025-06-06 13:37:09 +0200

    feat: add new handler, nodes

---
+++ Backend/src/Modules/Nodes/Nodes.Application/Entities/Nodes/Queries/ListFlaggedForDeletionNodes/ListFlaggedForDeletionNodesHandler.cs
@@ -0,0 +1,23 @@
+﻿using BuildingBlocks.Application.Data;
+using BuildingBlocks.Application.Pagination;
+using BuildingBlocks.Domain.Nodes.Node.Dtos;
+
+namespace Nodes.Application.Entities.Nodes.Queries.ListFlaggedForDeletionNodes;
+
+internal class ListFlaggedForDeletionNodesHandler(INodesService nodesService) : IQueryHandler<ListFlaggedForDeletionNodesQuery, ListFlaggedForDeletionNodesResponse>
+{
+    public async Task<ListFlaggedForDeletionNodesResponse> Handle(ListFlaggedForDeletionNodesQuery query, CancellationToken cancellationToken)
+    {
+        var pageIndex = query.PaginationRequest.PageIndex;
+        var pageSize = query.PaginationRequest.PageSize;
+
+        var nodes = await nodesService.ListFlaggedForDeletionNodesPaginated(pageIndex, pageSize, cancellationToken);
+
+        return new ListFlaggedForDeletionNodesResponse(
+            new PaginatedResult<NodeDto>(
+                pageIndex,
+                pageSize,
+                nodes.Count,
+                nodes));
+    }
+}
