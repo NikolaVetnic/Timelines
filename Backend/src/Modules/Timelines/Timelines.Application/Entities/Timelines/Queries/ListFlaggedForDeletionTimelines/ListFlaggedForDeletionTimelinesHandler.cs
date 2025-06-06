b78ba2175db1dd1e69c3f76080d35ebeabca6f20
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2025-06-06 14:16:51 +0200

    feat: add new handler, timelines

---
+++ Backend/src/Modules/Timelines/Timelines.Application/Entities/Timelines/Queries/ListFlaggedForDeletionTimelines/ListFlaggedForDeletionTimelinesHandler.cs
@@ -0,0 +1,22 @@
+﻿using BuildingBlocks.Application.Data;
+using BuildingBlocks.Application.Pagination;
+using BuildingBlocks.Domain.Timelines.Timeline.Dtos;
+
+namespace Timelines.Application.Entities.Timelines.Queries.ListFlaggedForDeletionTimelines;
+internal class ListFlaggedForDeletionTimelinesHandler(ITimelinesService timelinesService) : IQueryHandler<ListFlaggedForDeletionTimelinesQuery, ListFlaggedForDeletionTimelinesResponse>
+{
+    public async Task<ListFlaggedForDeletionTimelinesResponse> Handle(ListFlaggedForDeletionTimelinesQuery query, CancellationToken cancellationToken)
+    {
+        var pageIndex = query.PaginationRequest.PageIndex;
+        var pageSize = query.PaginationRequest.PageSize;
+
+        var timelines = await timelinesService.ListFlaggedForDeletionTimelinesPaginated(pageIndex, pageSize, cancellationToken);
+
+        return new ListFlaggedForDeletionTimelinesResponse(
+            new PaginatedResult<TimelineDto>(
+                pageIndex,
+                pageSize,
+                timelines.Count,
+                timelines));
+    }
+}
---
+++ Backend/src/Modules/Timelines/Timelines.Application/Entities/Timelines/Queries/ListFlaggedForDeletionTimelines/ListFlaggedForDeletionTimelinesQuery.cs
@@ -0,0 +1,8 @@
+﻿using BuildingBlocks.Application.Pagination;
+using BuildingBlocks.Domain.Timelines.Timeline.Dtos;
+
+namespace Timelines.Application.Entities.Timelines.Queries.ListFlaggedForDeletionTimelines;
+
+public record ListFlaggedForDeletionTimelinesQuery(PaginationRequest PaginationRequest) : IQuery<ListFlaggedForDeletionTimelinesResponse>;
+
+public record ListFlaggedForDeletionTimelinesResponse(PaginatedResult<TimelineDto> Nodes);
