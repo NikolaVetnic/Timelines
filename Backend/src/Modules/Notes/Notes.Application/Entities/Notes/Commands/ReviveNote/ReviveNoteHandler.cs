e2db283f9a7d3ece5095956c36235b95611b2612
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2025-06-09 13:33:07 +0200

    feat: implement revive note handler

---
+++ Backend/src/Modules/Notes/Notes.Application/Entities/Notes/Commands/ReviveNote/ReviveNoteHandler.cs
@@ -0,0 +1,19 @@
+﻿using BuildingBlocks.Application.Data;
+using Notes.Application.Entities.Notes.Exceptions;
+
+namespace Notes.Application.Entities.Notes.Commands.ReviveNote;
+
+internal class ReviveNoteHandler(INotesService notesService) : ICommandHandler<ReviveNoteCommand, ReviveNoteResponse>
+{
+    public async Task<ReviveNoteResponse> Handle(ReviveNoteCommand command, CancellationToken cancellationToken)
+    {
+        var note = await notesService.GetNoteBaseByIdAsync(command.Id, cancellationToken);
+
+        if (note is null)
+            throw new NoteNotFoundException(command.Id.ToString());
+
+        await notesService.ReviveNote(command.Id, cancellationToken);
+
+        return new ReviveNoteResponse(true);
+    }
+}
