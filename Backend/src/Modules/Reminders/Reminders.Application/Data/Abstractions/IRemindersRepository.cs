3de76e8e720b6742a796cdb7c8ea97bf886c273c
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2025-06-09 13:55:18 +0200

    refactor: remove excess method, reminders repo

--- Backend/src/Modules/Reminders/Reminders.Application/Data/Abstractions/IRemindersRepository.cs
+++ Backend/src/Modules/Reminders/Reminders.Application/Data/Abstractions/IRemindersRepository.cs
@@ -13,8 +13,6 @@ public interface IRemindersRepository
     Task<List<Reminder>> ListRemindersByNodeIdPaginatedAsync(NodeId nodeId, int pageIndex, int pageSize, CancellationToken cancellationToken);
     Task<Reminder> GetReminderByIdAsync(ReminderId reminderId, CancellationToken cancellationToken);
     Task<long> CountRemindersAsync(Expression<Func<Reminder, bool>> predicate, CancellationToken cancellationToken);
-    Task<long> CountAllRemindersAsync(CancellationToken cancellationToken);
-    Task<long> CountAllRemindersByNodeIdAsync(NodeId nodeId, CancellationToken cancellationToken);
 
     Task<IEnumerable<Reminder>> GetRemindersBelongingToNodeIdsAsync(IEnumerable<NodeId> nodeIds, CancellationToken cancellationToken);
 
