99ba458fe9d4b7b2159988c430b0dde2ec6681a8
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2025-06-09 13:32:50 +0200

    feat: add revive note command

---
+++ Backend/src/Modules/Notes/Notes.Application/Entities/Notes/Commands/ReviveNote/ReviveNoteCommand.cs
@@ -0,0 +1,20 @@
+﻿using BuildingBlocks.Domain.Notes.Note.ValueObjects;
+
+namespace Notes.Application.Entities.Notes.Commands.ReviveNote;
+
+public record ReviveNoteCommand(NoteId Id) : ICommand<ReviveNoteResponse>
+{
+    public ReviveNoteCommand(string Id) : this(NoteId.Of(Guid.Parse(Id))) { }
+}
+
+public record ReviveNoteResponse(bool NoteRevived);
+
+public class ReviveNoteCommandValidator : AbstractValidator<ReviveNoteCommand>
+{
+    public ReviveNoteCommandValidator()
+    {
+        RuleFor(x => x.Id)
+            .NotEmpty().WithMessage("Id is required.")
+            .Must(value => Guid.TryParse(value.ToString(), out _)).WithMessage("Id is not valid.");
+    }
+}
