e00e49fe5854e25b967cb49d8e53d706674e29f3
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2025-06-09 12:01:14 +0200

    feat: implement revive handler, file asset

---
+++ Backend/src/Modules/Files/Files.Application/Entities/Files/Commands/ReviveFileAsset/ReviveFileAssetHandler.cs
@@ -0,0 +1,19 @@
+﻿using BuildingBlocks.Application.Data;
+using Files.Application.Entities.Files.Exceptions;
+
+namespace Files.Application.Entities.Files.Commands.ReviveFileAsset;
+
+internal class ReviveFileAssetHandler(IFilesService filesService) : ICommandHandler<ReviveFileAssetCommand, ReviveFileAssetResult>
+{
+    public async Task<ReviveFileAssetResult> Handle(ReviveFileAssetCommand command, CancellationToken cancellationToken)
+    {
+        var fileAsset = await filesService.GetFileAssetBaseByIdAsync(command.Id, cancellationToken);
+
+        if (fileAsset is null)
+            throw new FileAssetNotFoundException(command.Id.ToString());
+
+        await filesService.ReviveFileAsset(command.Id, cancellationToken);
+
+        return new ReviveFileAssetResult(true);
+    }
+}
