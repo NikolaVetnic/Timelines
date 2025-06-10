7b3730d7aba9238768b9bf159eee43a1e047239e
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2025-06-10 07:52:34 +0200

    feat: implement revive reminder handler

---
+++ Backend/src/Modules/Reminders/Reminders.Application/Entities/Reminders/Commands/ReviveReminder/ReviveReminderHandler.cs
@@ -0,0 +1,19 @@
+﻿using BuildingBlocks.Application.Data;
+using Reminders.Application.Entities.Reminders.Exceptions;
+
+namespace Reminders.Application.Entities.Reminders.Commands.ReviveReminder;
+
+internal class ReviveReminderHandler(IRemindersService remindersService) : ICommandHandler<ReviveReminderCommand, ReviveReminderResult>
+{
+    public async Task<ReviveReminderResult> Handle(ReviveReminderCommand command, CancellationToken cancellationToken)
+    {
+        var reminder = await remindersService.GetReminderBaseByIdAsync(command.Id, cancellationToken);
+
+        if (reminder is null)
+            throw new ReminderNotFoundException(command.Id.ToString());
+
+        await remindersService.ReviveReminder(command.Id, cancellationToken);
+
+        return new ReviveReminderResult(true);
+    }
+}
