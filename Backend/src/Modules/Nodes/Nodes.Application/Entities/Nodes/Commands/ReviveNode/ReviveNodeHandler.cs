ff3a97325f9b6863658bdaf1a1d51b71497a4dee
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2025-06-10 09:02:07 +0200

    feat: implement revive handler, nodes

---
+++ Backend/src/Modules/Nodes/Nodes.Application/Entities/Nodes/Commands/ReviveNode/ReviveNodeHandler.cs
@@ -0,0 +1,19 @@
+﻿using Nodes.Application.Data.Abstractions;
+using Nodes.Application.Entities.Nodes.Exceptions;
+
+namespace Nodes.Application.Entities.Nodes.Commands.ReviveNode;
+
+internal class ReviveNodeHandler(INodesRepository nodesRepository) : ICommandHandler<ReviveNodeCommand, ReviveNodeResult>
+{
+    public async Task<ReviveNodeResult> Handle(ReviveNodeCommand command, CancellationToken cancellationToken)
+    {
+        var node = await nodesRepository.GetNodeByIdAsync(command.Id, cancellationToken);
+
+        if (node is null)
+            throw new NodeNotFoundException(command.Id.ToString());
+
+        await nodesRepository.ReviveNode(command.Id, cancellationToken);
+
+        return new ReviveNodeResult(true);
+    }
+}
