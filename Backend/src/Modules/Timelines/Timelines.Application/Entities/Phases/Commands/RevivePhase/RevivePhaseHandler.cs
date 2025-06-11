9bba8b169a0b98fef5531394b6f76ea218871a9a
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2025-06-11 13:18:34 +0200

    feat: implement revive handler, phases

---
+++ Backend/src/Modules/Timelines/Timelines.Application/Entities/Phases/Commands/RevivePhase/RevivePhaseHandler.cs
@@ -0,0 +1,14 @@
+﻿using Timelines.Application.Data.Abstractions;
+
+namespace Timelines.Application.Entities.Phases.Commands.RevivePhase;
+
+internal class RevivePhaseHandler(IPhasesRepository phasesRepository) : ICommandHandler<RevivePhaseCommand, RevivePhaseResult>
+{
+    public async Task<RevivePhaseResult> Handle(RevivePhaseCommand command, CancellationToken cancellationToken)
+    {
+        var phase = await phasesRepository.GetPhaseByIdAsync(command.Id, cancellationToken);
+        await phasesRepository.DeletePhaseAsync(phase.Id, cancellationToken);
+
+        return new RevivePhaseResult(true);
+    }
+}
