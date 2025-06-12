6004eb07be119616c3a87a3e9912b6384e0ba39b
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2025-06-12 09:57:45 +0200

    chore: small change

--- Backend/src/Modules/Timelines/Timelines.Application/Entities/Phases/Queries/ListFlaggedForDeletionPhases/ListFlaggedForDeletionPhasesQuery.cs
+++ Backend/src/Modules/Timelines/Timelines.Application/Entities/Phases/Queries/ListFlaggedForDeletionPhases/ListFlaggedForDeletionPhasesQuery.cs
@@ -3,7 +3,6 @@ using BuildingBlocks.Domain.Timelines.Phase.Dtos;
 
 namespace Timelines.Application.Entities.Phases.Queries.ListFlaggedForDeletionPhases;
 
-public record ListFlaggedForDeletionPhasesQuery(PaginationRequest PaginationRequest)
-    : IQuery<ListFlaggedForDeletionPhasesResult>;
+public record ListFlaggedForDeletionPhasesQuery(PaginationRequest PaginationRequest) : IQuery<ListFlaggedForDeletionPhasesResult>;
 
 public record ListFlaggedForDeletionPhasesResult(PaginatedResult<PhaseDto> Phases);
