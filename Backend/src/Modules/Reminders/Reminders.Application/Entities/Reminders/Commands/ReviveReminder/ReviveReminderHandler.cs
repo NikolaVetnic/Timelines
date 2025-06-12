4bf7a0ad2ca53e153a0b0374225f427fb1b717f7
Nikola Dmitrasinovic <nikola.dm@yahoo.com>
2025-06-12 10:30:18 +0200

    fix: use correct get method, revive handler

--- Backend/src/Modules/Reminders/Reminders.Application/Entities/Reminders/Commands/ReviveReminder/ReviveReminderHandler.cs
+++ Backend/src/Modules/Reminders/Reminders.Application/Entities/Reminders/Commands/ReviveReminder/ReviveReminderHandler.cs
@@ -1,18 +1,19 @@
 ﻿using BuildingBlocks.Application.Data;
+using Reminders.Application.Data.Abstractions;
 using Reminders.Application.Entities.Reminders.Exceptions;
 
 namespace Reminders.Application.Entities.Reminders.Commands.ReviveReminder;
 
-internal class ReviveReminderHandler(IRemindersService remindersService) : ICommandHandler<ReviveReminderCommand, ReviveReminderResult>
+internal class ReviveReminderHandler(IRemindersRepository remindersRepository) : ICommandHandler<ReviveReminderCommand, ReviveReminderResult>
 {
     public async Task<ReviveReminderResult> Handle(ReviveReminderCommand command, CancellationToken cancellationToken)
     {
-        var reminder = await remindersService.GetReminderBaseByIdAsync(command.Id, cancellationToken);
+        var reminder = await remindersRepository.GetFlaggedForDeletionReminderByIdAsync(command.Id, cancellationToken);
 
         if (reminder is null)
             throw new ReminderNotFoundException(command.Id.ToString());
 
-        await remindersService.ReviveReminder(command.Id, cancellationToken);
+        await remindersRepository.ReviveReminder(command.Id, cancellationToken);
 
         return new ReviveReminderResult(true);
     }
